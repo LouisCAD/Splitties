0dcc7bb58d4d42ed678d230684af2b7c5f1e1527
Louis Cognault <louis.cognault@gmail.com>
2016-08-13 19:26:54 +0200

    fix: Fixed expected packages names in tests

--- selectableviewgroups/src/androidTest/java/xyz/louiscad/selectableviewgroups/ExampleInstrumentedTest.java
+++ selectableviewgroups/src/androidTest/java/xyz/louiscad/selectableviewgroups/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("xyz.louiscad.reusables.test", appContext.getPackageName());
+        assertEquals("xyz.louiscad.selectableviewgroups.test", appContext.getPackageName());
     }
 }
