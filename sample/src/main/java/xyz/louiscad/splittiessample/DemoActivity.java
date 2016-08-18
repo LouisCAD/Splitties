d3410f6dc3183f3671409c98b9cf882fce9d61bf
Louis Cognault <louis.cognault@gmail.com>
2016-08-18 22:47:58 +0200

    feat: Create Demo Activity

---
+++ sample/src/main/java/xyz/louiscad/splittiessample/DemoActivity.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016. Louis Cognault Ayeva Derman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xyz.louiscad.splittiessample;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_demo)
+public class DemoActivity extends AppCompatActivity {
+
+    @ViewById(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @AfterViews
+    void initViews() {
+        setSupportActionBar(mToolbar);
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Click
+    void fabClicked(FloatingActionButton fab) {
+        Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+}
