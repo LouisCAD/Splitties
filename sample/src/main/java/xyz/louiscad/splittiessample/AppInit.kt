9d46e9c42b53ec91ebf551c6b797b3353f714097
Louis CAD <louis.cognault@gmail.com>
2017-04-16 01:20:39 +0200

    Add main thread check benchmark

--- sample/src/main/java/xyz/louiscad/splittiessample/AppInit.kt
+++ sample/src/main/java/xyz/louiscad/splittiessample/AppInit.kt
@@ -18,6 +18,8 @@ package xyz.louiscad.splittiessample
 
 import android.app.Application
 import android.content.ContentProvider
+import android.os.Looper
+import android.util.Log
 import timber.log.Timber
 
 /**
@@ -27,5 +29,71 @@ import timber.log.Timber
 object AppInit {
     init {
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        compareMainThreadChecks()
+    }
+
+    fun compareMainThreadChecks() {
+        val iterations = 1000
+        val mainLooper = Looper.getMainLooper()
+
+        var before = System.nanoTime()
+        for (i in 0..iterations - 1) {
+            check(Looper.myLooper() == mainLooper)
+        }
+        var duration = System.nanoTime() - before
+        Log.d("uiThread check duration", "looper duration:" + duration / 1000)
+
+        before = System.nanoTime()
+        for (i in 0..iterations - 1) {
+            check(Thread.currentThread().id == mainLooper.thread.id)
+        }
+        duration = System.nanoTime() - before
+        Log.d("uiThread check duration", "thread id duration:" + duration / 1000)
+
+        before = System.nanoTime()
+        for (i in 0..iterations - 1) {
+            check(mainLooper.thread == Thread.currentThread())
+        }
+        duration = System.nanoTime() - before
+        Log.d("uiThread check duration", "thread equals duration:" + duration / 1000)
+
+        before = System.nanoTime()
+        for (i in 0..iterations - 1) {
+            check(Thread.currentThread() === mainLooper.thread)
+        }
+        duration = System.nanoTime() - before
+        Log.d("uiThread check duration", "thread ref duration:" + duration / 1000)
+
+        val mainThread = mainLooper.thread
+
+        before = System.nanoTime()
+        for (i in 0..iterations - 1) {
+            check(Thread.currentThread().id == mainThread.id)
+        }
+        duration = System.nanoTime() - before
+        Log.d("uiThread check duration", "cached thread id duration:" + duration / 1000)
+
+        before = System.nanoTime()
+        for (i in 0..iterations - 1) {
+            check(mainThread == Thread.currentThread())
+        }
+        duration = System.nanoTime() - before
+        Log.d("uiThread check duration", "cached thread equals duration:" + duration / 1000)
+
+        before = System.nanoTime()
+        for (i in 0..iterations - 1) {
+            check(Thread.currentThread() === mainThread)
+        }
+        duration = System.nanoTime() - before
+        Log.d("uiThread check duration", "cached thread ref duration:" + duration / 1000)
+
+        val mainThreadId = mainLooper.thread.id
+
+        before = System.nanoTime()
+        for (i in 0..iterations - 1) {
+            check(Thread.currentThread().id == mainThreadId)
+        }
+        duration = System.nanoTime() - before
+        Log.d("uiThread check duration", "thread cached id duration:" + duration / 1000)
     }
 }
