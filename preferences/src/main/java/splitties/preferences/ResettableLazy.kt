c98ecfc58f690c1c8777fbdc4fa8cbc0f90e0569
Louis CAD <louis.cognault@gmail.com>
2017-04-16 23:27:09 +0200

    Add doc to ResettableLazy

--- preferences/src/main/java/splitties/preferences/ResettableLazy.kt
+++ preferences/src/main/java/splitties/preferences/ResettableLazy.kt
@@ -18,6 +18,19 @@ package splitties.preferences
 
 import kotlin.reflect.KProperty
 
+/**
+ * A Lazy delegate that keeps a reference to it's initializer and resets it's value if you set it's
+ * value with it's current value.
+ *
+ * Note that this doesn't support nullable values.
+ *
+ * Example:
+ * ```kotlin
+ * var myMember by ResettableLazy { Editor(preferences) }
+ * ...
+ * myMember = myMember // This line resets myMember. Next access will call the initializer again.
+ * ```
+ */
 internal class ResettableLazy<T : Any>(private val initializer: () -> T) {
 
     private var value: T? = null
