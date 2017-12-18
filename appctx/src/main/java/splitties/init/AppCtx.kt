49519c9d2fb35205d1c0603350cd8f8a2d6b37e1
Louis CAD <louis.cognault@gmail.com>
2017-12-18 23:49:50 +0100

    Add Context.canLeakMemory() + injectAsAppCtx() doc
    This commit also removes the Deprecation warning on injectAsAppCtx()
    while also broadening its scope from Application to any
    non leaking Context.

--- appctx/src/main/java/splitties/init/AppCtx.kt
+++ appctx/src/main/java/splitties/init/AppCtx.kt
@@ -17,14 +17,19 @@
 package splitties.init
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Application
+import android.app.Service
+import android.app.backup.BackupAgent
 import android.content.Context
+import android.content.ContextWrapper
+import android.view.ContextThemeWrapper
 
 /**
  * **WARNING!** Please, do not use this context if you rely on a scoped [Context] such as accessing
- * themed resources from an Activity.
+ * themed resources from an [Activity] or a [ContextThemeWrapper].
  *
- * This [Context] which is an instance of your [Application] class is initialized before
+ * This [Context] which by default is an instance of your [Application] class is initialized before
  * [Application.onCreate] is called (but after your [Application]'s constructor has run) thanks to
  * [AppCtxInitProvider] which is initialized by Android as early as possible when your app's
  * process is created.
@@ -46,15 +51,41 @@ internal fun initAppCtxWithReflection(): Context {
 }
 
 /**
- * Call this method from your [Application] subclass if you need to use [appCtx] outside the default
- * process and the reflection method throws an exception on some devices.
+ * **Usage of this method is discouraged** because the [appCtx] should be automatically initialized
+ * and setting it to a custom Context may cause unpredictable behavior in some parts of the app,
+ * especially if some libraries rely on the true applicationContext.
  *
- * If you ever use this method because of what is mentioned above, be sure to call this method
- * before you try to access [appCtx] or [directBootCtx].
+ * **However**, if your app needs to use [appCtx] outside of the default process and the reflection method throws an exception on some
+ * devices, you may find useful to call this method from your [Application.onCreate]. This method
+ * may also be useful if you need change the [appCtx] value with one from a configuration context
+ * with an overriden locale for example.
+ *
+ * **If you ever use this method because of non-default process, and throwing reflection init,
+ * be sure to call this method before you try to access [appCtx] or [directBootCtx].**
+ *
+ * Note that the input [Context] is checked for memory leak ability, and will throw if you try to
+ * pass an [Activity], a [Service], a [BackupAgent], a [Context] from another app or
+ * a [ContextWrapper] based on these classes.
  *
  * @see appCtx
+ * @see canLeakMemory
+ * @see Context.createConfigurationContext
  */
-@Deprecated("Should not be needed.", ReplaceWith(""), DeprecationLevel.WARNING)
-fun Application.injectAsAppCtx() {
+fun Context.injectAsAppCtx() {
+    require(!canLeakMemory()) { "The passed Context($this) would leak memory!" }
     internalCtx = this
+}
+
+/**
+ * This method will return true on [Context] implementations known to be able to leak memory.
+ * This includes [Activity], [Service], the lesser used and lesser known [BackupAgent], as well as
+ * any [ContextWrapper] having one of these as its base context.
+ */
+fun Context.canLeakMemory(): Boolean {
+    return when (this) {
+        is Application -> false
+        is Activity, is Service, is BackupAgent -> true
+        is ContextWrapper -> if (baseContext === this) true else baseContext.canLeakMemory()
+        else -> applicationContext === null
+    }
 }
