f6f337c39194ff76c2b348e224190a9a99893b71
Louis CAD <louis.cognault@gmail.com>
2018-01-06 02:44:10 +0100

    Add ColorResources

---
+++ resources/src/main/java/splitties/resources/ColorResources.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018. Louis Cognault Ayeva Derman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("NOTHING_TO_INLINE")
+
+package splitties.resources
+
+import android.app.Fragment
+import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Resources
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.M
+import android.support.annotation.ColorInt
+import android.support.annotation.ColorRes
+import android.view.View
+import splitties.init.appCtx
+import android.support.v4.app.Fragment as SupportFragment
+
+@ColorInt
+fun Context.color(@ColorRes colorRes: Int): Int = if (SDK_INT >= M) getColor(colorRes) else {
+    @Suppress("DEPRECATION")
+    resources.getColor(colorRes)
+}
+
+inline fun SupportFragment.color(@ColorRes colorRes: Int) = context!!.color(colorRes)
+inline fun Fragment.color(@ColorRes colorRes: Int) = activity.color(colorRes)
+inline fun View.color(@ColorRes colorRes: Int) = context.color(colorRes)
+/** Use this method for non configuration dependent resources when you don't have a [Context]
+ * or when you're calling it from an Activity or a Fragment member (as the Context is not
+ * initialized yet). */
+inline fun appColor(@ColorRes colorRes: Int) = appCtx.color(colorRes)
+
+fun Context.colorSL(@ColorRes colorRes: Int): ColorStateList {
+    return (if (SDK_INT >= M) getColorStateList(colorRes) else {
+        @Suppress("DEPRECATION")
+        resources.getColorStateList(colorRes)
+    }) ?: throw Resources.NotFoundException(resources.getResourceName(colorRes))
+}
+
+inline fun SupportFragment.colorSL(@ColorRes colorRes: Int) = context!!.colorSL(colorRes)
+inline fun Fragment.colorSL(@ColorRes colorRes: Int) = activity.colorSL(colorRes)
+inline fun View.colorSL(@ColorRes colorRes: Int) = context.colorSL(colorRes)
+/** Use this method for non configuration dependent resources when you don't have a [Context]
+ * or when you're calling it from an Activity or a Fragment member (as the Context is not
+ * initialized yet). */
+inline fun appColorSL(@ColorRes colorRes: Int) = appCtx.colorSL(colorRes)
