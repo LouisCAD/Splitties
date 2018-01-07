dfad4730cf063919c76586f6e9c901f35168c86f
Louis CAD <louis.cognault@gmail.com>
2018-01-07 01:42:35 +0100

    Create Arch Lifecycle module

---
+++ arch-lifecycle/src/main/java/splitties/arch/lifecycle/LiveData.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018. Louis Cognault Ayeva Derman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package splitties.arch.lifecycle
+
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.Transformations
+
+inline fun <T> LifecycleOwner.observe(liveData: LiveData<T>, crossinline observer: (t: T?) -> Unit) {
+    liveData.observe(this, Observer {
+        observer(it)
+    })
+}
+
+inline fun <T : Any> LifecycleOwner.observeNotNull(liveData: LiveData<T>, crossinline observer: (t: T) -> Unit) {
+    liveData.observe(this, Observer {
+        if (it != null) observer(it)
+    })
+}
+
+@JvmName("observeWithLiveDataOfNullable")
+inline fun <T : Any> LifecycleOwner.observeNotNull(liveData: LiveData<T?>, crossinline observer: (t: T) -> Unit) {
+    liveData.observe(this, Observer {
+        if (it != null) observer(it)
+    })
+}
+
+/**
+ * Applies the given function on the main thread to each value emitted by source
+ * LiveData and returns LiveData, which emits resulting values.
+ *
+ * The given function [transform] will be **executed on the main thread**.
+ *
+ * @param transform   a function to apply
+ * @param X           a type of source LiveData
+ * @param Y           a type of resulting LiveData.
+ * @return            a LiveData which emits resulting values
+ */
+inline fun <X, Y> LiveData<X>.map(crossinline transform: (X?) -> Y): LiveData<Y> = Transformations.map(this, { input ->
+    transform(input)
+})!!
---
+++ arch-lifecycle/src/main/java/splitties/arch/lifecycle/ViewModel.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018. Louis Cognault Ayeva Derman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package splitties.arch.lifecycle
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import splitties.checkedlazy.uiLazy
+
+inline fun <reified VM : ViewModel> FragmentActivity.activityScope() = uiLazy {
+    ViewModelProviders.of(this).get(VM::class.java)
+}
+
+inline fun <reified VM : ViewModel> Fragment.activityScope() = uiLazy {
+    ViewModelProviders.of(this).get(VM::class.java)
+}
+
+inline fun <reified VM : ViewModel> FragmentActivity.activityScope(factory: ViewModelProvider.Factory) = uiLazy {
+    ViewModelProviders.of(this, factory).get(VM::class.java)
+}
+
+inline fun <reified VM : ViewModel> Fragment.activityScope(factory: ViewModelProvider.Factory) = uiLazy {
+    ViewModelProviders.of(this, factory).get(VM::class.java)
+}
