119fbabbd7830063acdac8f85ac8442503a8225a
Louis CAD <louis.cognault@gmail.com>
2018-01-15 22:17:56 +0100

    Create CoordinatorLayout file

---
+++ viewdsl-design/src/main/java/splitties/viewdsl/design/CoordinatorLayout.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018. Louis Cognault Ayeva Derman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("NOTHING_TO_INLINE")
+
+package splitties.viewdsl.design
+
+import android.support.design.widget.CoordinatorLayout
+import android.view.Gravity
+import splitties.viewdsl.core.wrapContent
+
+inline fun CoordinatorLayout.lParams(
+        width: Int = wrapContent,
+        height: Int = wrapContent,
+        gravity: Int = Gravity.NO_GRAVITY,
+        initParams: CoordinatorLayout.LayoutParams.() -> Unit
+) = CoordinatorLayout.LayoutParams(width, height).also { it.gravity = gravity }.apply(initParams)
+
+inline fun CoordinatorLayout.lParams(
+        width: Int = wrapContent,
+        height: Int = wrapContent,
+        gravity: Int = Gravity.NO_GRAVITY
+) = CoordinatorLayout.LayoutParams(width, height).also { it.gravity = gravity }
+
+inline fun CoordinatorLayout.lParams(
+        width: Int = wrapContent,
+        height: Int = wrapContent
+) = CoordinatorLayout.LayoutParams(width, height)
