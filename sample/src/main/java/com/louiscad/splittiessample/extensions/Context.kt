e86d489766ae29c016cf1e58de37a3a546363713
Louis CAD <louis.cognault@gmail.com>
2018-03-24 20:36:44 +0100

    Remove useless overload

--- sample/src/main/java/com/louiscad/splittiessample/extensions/Context.kt
+++ sample/src/main/java/com/louiscad/splittiessample/extensions/Context.kt
@@ -20,8 +20,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 
-inline fun <reified A : Activity> Context.start() = startActivity(Intent(this, A::class.java))
-
 inline fun <reified A : Activity> Context.start(configIntent: Intent.() -> Unit = {}) {
     startActivity(Intent(this, A::class.java).apply(configIntent))
 }
