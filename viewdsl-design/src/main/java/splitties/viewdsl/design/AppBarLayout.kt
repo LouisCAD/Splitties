ff4b5079ddb2e67dd58ebbebbafde22e5c9092ad
Louis CAD <louis.cognault@gmail.com>
2018-03-24 10:29:45 +0100

    Fold line which was more than 100 chars

--- viewdsl-design/src/main/java/splitties/viewdsl/design/AppBarLayout.kt
+++ viewdsl-design/src/main/java/splitties/viewdsl/design/AppBarLayout.kt
@@ -24,7 +24,8 @@ import splitties.viewdsl.core.wrapContent
 import android.support.design.widget.AppBarLayout.LayoutParams as LP
 
 @Suppress("unused") val AppBarLayout.SCROLL get() = LP.SCROLL_FLAG_SCROLL
-@Suppress("unused") val AppBarLayout.EXIT_UNTIL_COLLAPSED get() = LP.SCROLL_FLAG_EXIT_UNTIL_COLLAPSED
+@Suppress("unused") val AppBarLayout.EXIT_UNTIL_COLLAPSED
+    get() = LP.SCROLL_FLAG_EXIT_UNTIL_COLLAPSED
 @Suppress("unused") val AppBarLayout.ENTER_ALWAYS get() = LP.SCROLL_FLAG_ENTER_ALWAYS
 @Suppress("unused") val AppBarLayout.ENTER_ALWAYS_COLLAPSED: Int
     get() = LP.SCROLL_FLAG_ENTER_ALWAYS_COLLAPSED
