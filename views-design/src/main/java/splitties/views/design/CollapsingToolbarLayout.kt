b07d5f430b6c8420a2e68c085cf54c1acf7ee298
Louis CAD <louis.cognault@gmail.com>
2018-03-31 16:27:20 +0200

    Move contentScrimColor extension to views-design

--- views-design/src/main/java/splitties/views/design/CollapsingToolbarLayout.kt
+++ views-design/src/main/java/splitties/views/design/CollapsingToolbarLayout.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.louiscad.splittiessample.extensions
+package splitties.views.design
 
 import android.support.annotation.ColorInt
 import android.support.design.widget.CollapsingToolbarLayout
---
+++ views-design/src/main/java/splitties/views/design/NoGetter.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018. Louis Cognault Ayeva Derman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package splitties.views.design
+
+@PublishedApi internal const val NO_GETTER = "Property does not have a getter"
+
+/**
+ * Usage example:
+ * `@Deprecated(NO_GETTER, level = DeprecationLevel.HIDDEN) get() = noGetter`
+ */
+@PublishedApi
+internal inline val noGetter: Nothing get() = throw UnsupportedOperationException(NO_GETTER)
