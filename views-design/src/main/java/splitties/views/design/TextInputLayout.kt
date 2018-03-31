b5e6adddabf50e45dceed58e14d64f1784c696ef
Louis CAD <louis.cognault@gmail.com>
2018-03-31 16:26:54 +0200

    Move TextInputLayout extensions to views-design

--- views-design/src/main/java/splitties/views/design/TextInputLayout.kt
+++ views-design/src/main/java/splitties/views/design/TextInputLayout.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.louiscad.splittiessample.extensions.views
+package splitties.views.design
 
 import android.support.design.widget.TextInputLayout
 import android.widget.EditText
