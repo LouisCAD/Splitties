135d1eea31d2992b57174e7f70934b1393f80b3a
Louis CAD <louis.cognault@gmail.com>
2018-04-12 00:38:21 +0200

    Add switchMap + notNull variant, and mapNotNull
    Resolves #70

--- arch-lifecycle/src/main/java/splitties/arch/lifecycle/LiveData.kt
+++ arch-lifecycle/src/main/java/splitties/arch/lifecycle/LiveData.kt
@@ -57,3 +57,19 @@ inline fun <T : Any> LifecycleOwner.observeNotNull(
 inline fun <X, Y> LiveData<X>.map(
         crossinline transform: (X?) -> Y
 ): LiveData<Y> = Transformations.map(this, { input -> transform(input) })
+
+inline fun <X, Y> LiveData<X>.mapNotNull(
+        crossinline transform: (X) -> Y
+): LiveData<Y> = Transformations.map(this, { input: X? ->
+    input?.let { transform(it) }
+})
+
+inline fun <X, Y> LiveData<X>.switchMap(
+        crossinline transform: (X?) -> LiveData<Y>?
+): LiveData<Y> = Transformations.switchMap(this, { input -> transform(input) })
+
+inline fun <X, Y> LiveData<X>.switchMapNotNull(
+        crossinline transform: (X) -> LiveData<Y>?
+): LiveData<Y> = Transformations.switchMap(this, { input: X? ->
+    input?.let { transform(it) }
+})
