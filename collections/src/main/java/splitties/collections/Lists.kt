a1bf90508a4af947c73b2f01da8e5efc0ed6e3f0
Louis CAD <louis.cognault@gmail.com>
2018-05-20 20:48:44 +0200

    Add forEachWithIndex method in collections module

--- collections/src/main/java/splitties/collections/Lists.kt
+++ collections/src/main/java/splitties/collections/Lists.kt
@@ -18,7 +18,7 @@ package splitties.collections
 import java.util.*
 
 /**
- * Iterate the receiver [List] using an index instead of an [Iterator] like [forEach] would do.
+ * Iterates the receiver [List] using an index instead of an [Iterator] like [forEach] would do.
  * Using this function saves an [Iterator] allocation, which is good for immutable lists or usages
  * confined to a single thread like UI thread only use.
  * However, this method will not detect concurrent modification, except if the size of the list
@@ -31,5 +31,22 @@ inline fun <T> List<T>.forEachByIndex(action: (T) -> Unit) {
     for (i in 0..lastIndex) {
         if (size != initialSize) throw ConcurrentModificationException()
         action(get(i))
+    }
+}
+
+/**
+ * Iterates the receiver [List] using an index instead of an [Iterator] like [forEachIndexed] would
+ * do. Using this function saves an [Iterator] allocation, which is good for immutable lists or
+ * usages confined to a single thread like UI thread only use.
+ * However, this method will not detect concurrent modification, except if the size of the list
+ * changes on an iteration as a result, which may lead to unpredictable behavior.
+ *
+ * @param action the action to invoke on each list element.
+ */
+inline fun <T> List<T>.forEachWithIndex(action: (Int, T) -> Unit) {
+    val initialSize = size
+    for (i in 0..lastIndex) {
+        if (size != initialSize) throw ConcurrentModificationException()
+        action(i, get(i))
     }
 }
