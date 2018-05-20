cd77b4bc22f3969b7fc0ff267c8d65712327bbfc
Louis CAD <louis.cognault@gmail.com>
2018-05-20 19:34:38 +0200

    Check size does not change in forEachByIndex

--- collections/src/main/java/splitties/collections/Lists.kt
+++ collections/src/main/java/splitties/collections/Lists.kt
@@ -15,15 +15,21 @@
  */
 package splitties.collections
 
+import java.util.*
+
 /**
  * Iterate the receiver [List] using an index instead of an [Iterator] like [forEach] would do.
  * Using this function saves an [Iterator] allocation, which is good for immutable lists or usages
  * confined to a single thread like UI thread only use.
- * However, this method will not detect concurrent modification, which may lead to unpredictable
- * behavior.
+ * However, this method will not detect concurrent modification, except if the size of the list
+ * changes on an iteration as a result, which may lead to unpredictable behavior.
  *
  * @param action the action to invoke on each list element.
  */
 inline fun <T> List<T>.forEachByIndex(action: (T) -> Unit) {
-    for (i in 0..lastIndex) action(get(i))
+    val initialSize = size
+    for (i in 0..lastIndex) {
+        if (size != initialSize) throw ConcurrentModificationException()
+        action(get(i))
+    }
 }
