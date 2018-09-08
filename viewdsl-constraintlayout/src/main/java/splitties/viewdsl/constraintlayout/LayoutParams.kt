961137ba80b271f21f4285fd83f99b9142874112
Louis CAD <louis.cognault@gmail.com>
2018-09-08 03:12:46 +0200

    Auto-add generated id for ConstraintLayout children if needed
    Closes #117

--- viewdsl-constraintlayout/src/main/java/splitties/viewdsl/constraintlayout/LayoutParams.kt
+++ viewdsl-constraintlayout/src/main/java/splitties/viewdsl/constraintlayout/LayoutParams.kt
@@ -19,6 +19,7 @@
 package splitties.viewdsl.constraintlayout
 
 import android.view.View
+import splitties.views.generateViewId
 import android.support.constraint.ConstraintLayout.LayoutParams as LP
 
 @Suppress("unused") inline val LP.parentId get() = LP.PARENT_ID
@@ -65,11 +66,14 @@ inline fun LP.rightOfParent() {
     rightToRight = parentId
 }
 
-@PublishedApi internal val View.idOrThrow: Int
-    get() = id.also { check(it != View.NO_ID) { "This view has no id!" } }
+@PublishedApi internal val View.existingOrNewId: Int
+    get() {
+        val currentId = id
+        return if (currentId == View.NO_ID) generateViewId().also { id = it } else currentId
+    }
 
 inline fun LP.centerOn(view: View) {
-    val id = view.idOrThrow
+    val id = view.existingOrNewId
     topToTop = id
     leftToLeft = id
     bottomToBottom = id
@@ -77,65 +81,65 @@ inline fun LP.centerOn(view: View) {
 }
 
 inline fun LP.topToTopOf(view: View) {
-    topToTop = view.idOrThrow
+    topToTop = view.existingOrNewId
 }
 
 inline fun LP.topToBottomOf(view: View) {
-    topToBottom = view.idOrThrow
+    topToBottom = view.existingOrNewId
 }
 
 inline fun LP.bottomToTopOf(view: View) {
-    bottomToTop = view.idOrThrow
+    bottomToTop = view.existingOrNewId
 }
 
 inline fun LP.bottomToBottomOf(view: View) {
-    bottomToBottom = view.idOrThrow
+    bottomToBottom = view.existingOrNewId
 }
 
 inline fun LP.baselineToBaselineOf(view: View) {
-    baselineToBaseline = view.idOrThrow
+    baselineToBaseline = view.existingOrNewId
 }
 
 inline fun LP.startToStartOf(view: View) {
-    startToStart = view.idOrThrow
+    startToStart = view.existingOrNewId
 }
 
 inline fun LP.startToEndOf(view: View) {
-    startToEnd = view.idOrThrow
+    startToEnd = view.existingOrNewId
 }
 
 inline fun LP.endToStartOf(view: View) {
-    endToStart = view.idOrThrow
+    endToStart = view.existingOrNewId
 }
 
 inline fun LP.endToEndOf(view: View) {
-    endToEnd = view.idOrThrow
+    endToEnd = view.existingOrNewId
 }
 
 inline fun LP.leftToLeftOf(view: View) {
-    leftToLeft = view.idOrThrow
+    leftToLeft = view.existingOrNewId
 }
 
 inline fun LP.leftToRightOf(view: View) {
-    leftToRight = view.idOrThrow
+    leftToRight = view.existingOrNewId
 }
 
 inline fun LP.rightToRightOf(view: View) {
-    rightToRight = view.idOrThrow
+    rightToRight = view.existingOrNewId
 }
 
 inline fun LP.rightToLeftOf(view: View) {
-    rightToLeft = view.idOrThrow
+    rightToLeft = view.existingOrNewId
 }
 
 inline fun LP.alignVerticallyOn(view: View) {
-    val id = view.idOrThrow
+    val id = view.existingOrNewId
     topToTop = id
     bottomToBottom = id
 }
 
 inline fun LP.alignHorizontallyOn(view: View) {
-    val id = view.idOrThrow
+    val id = view.existingOrNewId
     leftToLeft = id
     rightToRight = id
 }
