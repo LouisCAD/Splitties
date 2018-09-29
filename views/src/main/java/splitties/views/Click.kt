7f5ddde322215d5cdcda394ae98aa4a93208fd48
Louis CAD <louis.cognault@gmail.com>
2018-09-29 21:06:28 +0200

    Add onLongClick() extension function for View

--- views/src/main/java/splitties/views/Click.kt
+++ views/src/main/java/splitties/views/Click.kt
@@ -26,3 +26,16 @@ import android.view.View
  * If this View is not clickable, it becomes clickable.
  */
 inline fun View.onClick(crossinline block: () -> Unit) = setOnClickListener { block() }
+
+/**
+ * Register the [block] lambda as [View.OnLongClickListener] to this View.
+ * By default, [consume] is set to true because it's the most common use case, but you can set it
+ * to false.
+ * If you want to return a value dynamically, use [View.setOnLongClickListener] instead.
+ *
+ * If this view is not long clickable, it becomes long clickable.
+ */
+inline fun View.onLongClick(
+        consume: Boolean = true,
+        crossinline block: () -> Unit
+) = setOnLongClickListener { block(); consume }
