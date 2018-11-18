d545967742ba472d365fd0b8270aac1f0eaf8367
Louis CAD <louis.cognault@gmail.com>
2018-11-18 11:34:33 +0100

    Enforce read-only for Activity.withExtras
    This commit also adds documentation to withExtras, putExtras and with
    from the Bundle split, and it simplifies BundleSpec internal API.

--- bundle/src/main/java/splitties/bundle/BundleSpec.kt
+++ bundle/src/main/java/splitties/bundle/BundleSpec.kt
@@ -19,14 +19,26 @@
 package splitties.bundle
 
 import android.os.Bundle
-import android.support.annotation.MainThread
-import android.support.annotation.WorkerThread
+import splitties.mainthread.isMainThread
 
 open class BundleSpec {
-    @get:WorkerThread
-    internal val bundleByThread by lazy { ThreadLocal<Bundle?>() }
-    @Suppress("ANNOTATION_TARGETS_NON_EXISTENT_ACCESSOR")
-    @get:MainThread
-    @JvmField
-    internal var currentBundle: Bundle? = null
+
+    @PublishedApi
+    internal var currentBundle: Bundle?
+        get() = if (isMainThread) currentBundleMainThread else currentBundleByThread.get()
+        set(value) {
+            if (isMainThread) currentBundleMainThread = value else currentBundleByThread.set(value)
+        }
+
+    @PublishedApi
+    internal var isReadOnly: Boolean
+        get() = if (isMainThread) isReadOnlyMainThread else isReadOnlyByThread.get() ?: false
+        set(value) {
+            if (isMainThread) isReadOnlyMainThread = value else isReadOnlyByThread.set(value)
+        }
+
+    private var currentBundleMainThread: Bundle? = null
+    private val currentBundleByThread by lazy { ThreadLocal<Bundle?>() }
+    private var isReadOnlyMainThread = false
+    private val isReadOnlyByThread by lazy { ThreadLocal<Boolean>() }
 }
