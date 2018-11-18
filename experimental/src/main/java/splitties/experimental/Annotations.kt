8bf3b15b3e724c023fb0fa79d9cac96fd02f78f8
Louis CAD <louis.cognault@gmail.com>
2018-11-18 21:10:15 +0100

    Add InternalSplittiesApi annotation
    For future usage.

--- experimental/src/main/java/splitties/experimental/Annotations.kt
+++ experimental/src/main/java/splitties/experimental/Annotations.kt
@@ -4,3 +4,8 @@ package splitties.experimental
 @Retention(value = AnnotationRetention.BINARY)
 @Experimental(level = Experimental.Level.WARNING)
 annotation class ExperimentalSplittiesApi
+
+@MustBeDocumented
+@Retention(value = AnnotationRetention.BINARY)
+@Experimental(level = Experimental.Level.ERROR)
+annotation class InternalSplittiesApi
