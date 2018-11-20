2c79795b2b931904461cd1fa3f068e29a4f57d44
Louis CAD <louis.cognault@gmail.com>
2018-11-20 18:10:01 +0100

    Add coroutines friendly LazyRoom class + extension

---
+++ sample/src/main/java/com/louiscad/splittiessample/extensions/room/SuspendLazyRoom.kt
@@ -0,0 +1,18 @@
+package com.louiscad.splittiessample.extensions.room
+
+import android.arch.persistence.room.RoomDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import splitties.arch.room.DbConfig
+import splitties.arch.room.roomDb
+
+inline fun <reified DB : RoomDatabase> roomDataBase(
+    name: String,
+    crossinline config: DbConfig<DB> = {}
+): LazyRoom<DB> = LazyRoom(lazy { roomDb(name, config) })
+
+class LazyRoom<T : RoomDatabase> @PublishedApi internal constructor(private val lazyDb: Lazy<T>) {
+    suspend operator fun invoke(): T = with(lazyDb) {
+        if (isInitialized()) value else withContext(Dispatchers.IO) { value }
+    }
+}
