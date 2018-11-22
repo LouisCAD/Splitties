94984efd5c093c9e88b466bc0f3b30bae51fde45
Louis CAD <louis.cognault@gmail.com>
2018-11-22 15:00:27 +0100

    Replace LazyRoom by more generic SuspendLazy

---
+++ sample/src/main/java/com/louiscad/splittiessample/extensions/coroutines/SuspendLazy.kt
@@ -0,0 +1,15 @@
+package com.louiscad.splittiessample.extensions.coroutines
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class SuspendLazy<T>(
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Default,
+    initializer: () -> T
+) {
+    private val lazyValue = lazy(initializer)
+    suspend operator fun invoke(): T = with(lazyValue) {
+        if (isInitialized()) value else withContext(dispatcher) { value }
+    }
+}
--- sample/src/main/java/com/louiscad/splittiessample/extensions/coroutines/SuspendLazyRoom.kt
+++ sample/src/main/java/com/louiscad/splittiessample/extensions/coroutines/SuspendLazyRoom.kt
@@ -2,18 +2,10 @@ package com.louiscad.splittiessample.extensions.coroutines
 
 import android.arch.persistence.room.RoomDatabase
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import splitties.arch.room.DbConfig
 import splitties.arch.room.roomDb
 
 inline fun <reified DB : RoomDatabase> roomDataBase(
     name: String,
     crossinline config: DbConfig<DB> = {}
-): LazyRoom<DB> =
-    LazyRoom(lazy { roomDb(name, config) })
-
-class LazyRoom<T : RoomDatabase> @PublishedApi internal constructor(private val lazyDb: Lazy<T>) {
-    suspend operator fun invoke(): T = with(lazyDb) {
-        if (isInitialized()) value else withContext(Dispatchers.IO) { value }
-    }
-}
+): SuspendLazy<DB> = SuspendLazy(Dispatchers.IO) { roomDb(name, config) }
