b700906620affe9f213c76c89b72797c24c6205f
Louis CAD <louis.cognault@gmail.com>
2019-01-05 00:01:52 +0100

    Finish initial implementation of Releasing.kts

--- Releasing.kts
+++ Releasing.kts
@@ -32,7 +32,29 @@ fun String.execute(workingDir: File = dir): String {
     return proc.inputStream.bufferedReader().readText()
 }
 
-println("git describe --dirty".execute())
+fun String.executeAndPrint(workingDir: File = dir) {
+    val parts = this.split("\\s".toRegex())
+    val proc = ProcessBuilder(*parts.toTypedArray())
+        .directory(workingDir)
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start()
+
+    proc.waitFor(60, TimeUnit.MINUTES)
+    val reader = proc.inputStream.bufferedReader()
+    while (true) {
+        reader.readLine()?.also { line -> println(line) } ?: break
+    }
+}
+
+fun requestUserConfirmation(yesNoQuestion: String) {
+    println("$yesNoQuestion Y/n")
+    readLine()?.trimEnd().let { input ->
+        if (input != "Y" && !"yes".equals(input, ignoreCase = true)) {
+            println("Process aborted."); return
+        }
+    }
+}
 
 fun releasingNonSnapshot() {
     val currentBranch = "git rev-parse --abbrev-ref HEAD".execute().trimEnd()
@@ -42,12 +64,15 @@ fun releasingNonSnapshot() {
     val libraryVersionLine = versionsFile.readLines().singleOrNull { line ->
         line.startsWith(libVersionLineStart)
     } ?: throw IllegalStateException("Library version line not found.")
-    println(
-        "Current version: ${libraryVersionLine.substring(
-            startIndex = libVersionLineStart.length,
-            endIndex = libraryVersionLine.lastIndex
-        )}"
-    )
+    val currentSnapshotVersion = libraryVersionLine.substring(
+        startIndex = libVersionLineStart.length,
+        endIndex = libraryVersionLine.lastIndex
+    ).also { versionName ->
+        check(versionName.endsWith("-SNAPSHOT")) {
+            "Version in ${versionsFile.path} should be a `-SNAPSHOT` version."
+        }
+    }
+    println("Current version: $currentSnapshotVersion")
     println("Please enter the name of the new version you want to release:")
     val newVersion = readLine()?.trimEnd().also { input ->
         when {
@@ -66,35 +91,67 @@ fun releasingNonSnapshot() {
         check("v$input" !in existingVersions) { "This version already exists!" }
     }!!
     println("New version: \"$newVersion\"")
-    println("Confirm? Y/n")
-    readLine()?.trimEnd().let { input ->
-        if (input != "Y" && !"yes".equals(input, ignoreCase = true)) {
-            println("Process aborted."); return
-        }
+    requestUserConfirmation("Confirm?")
+    val versionsFileTextBeforeEdits = versionsFile.readText()
+    versionsFile.writeText(
+       versionsFileTextBeforeEdits.replace(
+            oldValue = libraryVersionLine,
+            newValue = "$libVersionLineStart$newVersion\""
+        )
+    )
+    println("Update the `README.md` with the new version and any other changes.")
+    requestUserConfirmation("Done?")
+    println("Update the `CHANGELOG.md` for the impending release.")
+    requestUserConfirmation("Done?")
+    "git commit -am \"Prepare for release $newVersion\"".executeAndPrint()
+    "git tag -a v$newVersion -m \"Version $newVersion\"".executeAndPrint()
+    val cleanAndUploadCommand = "./gradlew clean bintrayUpload"
+    println("Running `$cleanAndUploadCommand`")
+    cleanAndUploadCommand.executeAndPrint()
+    println("Please check upload succeded.")
+    val pushToOriginCommand = "git push origin"
+    println("Will now run $pushToOriginCommand")
+    requestUserConfirmation("Continue?")
+    pushToOriginCommand.executeAndPrint()
+    println("Create a pull request from the `develop` to the `master` branch on GitHub for the new version, if not already done.")
+    requestUserConfirmation("Done?")
+    println("Sign in on Bintray and publish the packages.")
+    requestUserConfirmation("Done?")
+    val pushToOriginWithTagsCommand = "git push origin --tags"
+    println("Will now run $pushToOriginWithTagsCommand")
+    requestUserConfirmation("Continue?")
+    println("Merge the pull request for the new version on GitHub.")
+    requestUserConfirmation("Done?")
+    println("Publish release on GitHub.")
+    requestUserConfirmation("Done?")
+    println("Will now checkout the `master` branch, pull from GitHub (origin) to update the local `master` branch.")
+    requestUserConfirmation("Continue?")
+    "git checkout master".executeAndPrint()
+    "git pull origin".executeAndPrint()
+    println("About to checkout the develop branch (and update it from master for merge commits).")
+    requestUserConfirmation("Continue?")
+    "git checkout develop".executeAndPrint()
+    "git merge master".executeAndPrint()
+    println("Let's update the library for next development version.")
+    println("If you want to keep using $currentSnapshotVersion, enter an empty line.")
+    println("Otherwise, enter the name of the next target version (`-SNAPSHOT` will be added automatically)")
+    val nextDevVersion: String = readLine().let { input ->
+        if (input.isNullOrBlank()) currentSnapshotVersion else "$input-SNAPSHOT"
     }
-    versionsFile.writeText(versionsFile.readText().replace(
-        oldValue = libraryVersionLine,
-        newValue = "$libVersionLineStart$newVersion\""
-    ))
-    /*
-    3. Update the `README.md` with the new version.
-        4. Update the `CHANGELOG.md` for the impending release.
-        5. Run `git commit -am "Prepare for release X.Y.Z"` (where X.Y.Z is the new version).
-        6. `git tag -a vX.Y.Z -m "Version X.Y.Z"` (where X.Y.Z is the new version).
-        7. Run `./gradlew clean bintrayUpload`.
-    8. Run `git push origin`.
-    9. Create a pull request from the `develop` to the `master` branch on GitHub for the new version, if not already done.
-    10. Sign in on Bintray and publish the packages.
-    11. Run `git push origin --tags`.
-    12. Merge the pull request for the new version on GitHub.
-        13. Publish release on GitHub.
-    14. Checkout the `master` branch.
-    15. Pull from GitHub repository to update the local `master` branch.
-    16. Checkout the `develop` branch.
-    17. Change the `thisLibrary` constant in the
-            [ProjectVersions](buildSrc/src/main/kotlin/ProjectVersions.kt) file back to a SNAPSHOT version.
-        18. Run `git commit -am "Prepare next development version."`.
-    19. Run `git push origin`.*/
+    versionsFile.writeText(
+        versionsFileTextBeforeEdits.replace(
+            oldValue = libraryVersionLine,
+            newValue = "$libVersionLineStart$nextDevVersion\""
+        )
+    )
+    println("${versionsFile.path} has been edited with next developement version ($nextDevVersion).")
+    val nextDevVersionCommitCommand = "git commit -am \"Prepare next development version.\""
+    requestUserConfirmation("Will run $nextDevVersionCommitCommand Continue?")
+    nextDevVersionCommitCommand.executeAndPrint()
+
+    requestUserConfirmation("Finally the last step: Running: `$pushToOriginCommand`. Continue?")
+    pushToOriginCommand.executeAndPrint()
+    println("All Done! Let's brag about this new release!!")
 }
 
 fun openUrl(url: String) {
