3a5a3e682cb96e63573d42049ebf2206d3c23b98
Louis CAD <louis.cognault@gmail.com>
2019-01-15 12:31:51 +0100

    Add contracts to view visibility scope extensions

--- sample/src/main/kotlin/com/louiscad/splittiessample/extensions/View.kt
+++ sample/src/main/kotlin/com/louiscad/splittiessample/extensions/View.kt
@@ -3,24 +3,39 @@ package com.louiscad.splittiessample.extensions
 import android.view.View
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
-inline fun <R> View.visibleInScope(finallyInvisible: Boolean = false, block: () -> R) = try {
-    isVisible = true
-    block()
-} finally {
-    visibility = if (finallyInvisible) View.INVISIBLE else View.GONE
+@ExperimentalContracts
+inline fun <R> View.visibleInScope(finallyInvisible: Boolean = false, block: () -> R): R {
+    contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+    return try {
+        isVisible = true
+        block()
+    } finally {
+        visibility = if (finallyInvisible) View.INVISIBLE else View.GONE
+    }
 }
 
-inline fun <R> View.goneInScope(block: () -> R) = try {
-    isVisible = false
-    block()
-} finally {
-    isVisible = true
+@ExperimentalContracts
+inline fun <R> View.goneInScope(block: () -> R): R {
+    contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+    return try {
+        isVisible = false
+        block()
+    } finally {
+        isVisible = true
+    }
 }
 
-inline fun <R> View.invisibleInScope(block: () -> R) = try {
-    isInvisible = true
-    block()
-} finally {
-    isInvisible = false
+@ExperimentalContracts
+inline fun <R> View.invisibleInScope(block: () -> R): R {
+    contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+    return try {
+        isInvisible = true
+        block()
+    } finally {
+        isInvisible = false
+    }
 }
