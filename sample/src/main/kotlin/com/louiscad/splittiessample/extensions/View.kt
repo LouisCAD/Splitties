a31771097c5e4fe2076faa494842790c05eb808f
Louis CAD <louis.cognault@gmail.com>
2019-01-15 12:27:21 +0100

    Return last expression in View visibility scope extensions

--- sample/src/main/kotlin/com/louiscad/splittiessample/extensions/View.kt
+++ sample/src/main/kotlin/com/louiscad/splittiessample/extensions/View.kt
@@ -4,21 +4,21 @@ import android.view.View
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 
-inline fun View.visibleInScope(finallyInvisible: Boolean = false, block: () -> Unit) = try {
+inline fun <R> View.visibleInScope(finallyInvisible: Boolean = false, block: () -> R) = try {
     isVisible = true
     block()
 } finally {
     visibility = if (finallyInvisible) View.INVISIBLE else View.GONE
 }
 
-inline fun View.goneInScope(block: () -> Unit) = try {
+inline fun <R> View.goneInScope(block: () -> R) = try {
     isVisible = false
     block()
 } finally {
     isVisible = true
 }
 
-inline fun View.invisibleInScope(block: () -> Unit) = try {
+inline fun <R> View.invisibleInScope(block: () -> R) = try {
     isInvisible = true
     block()
 } finally {
