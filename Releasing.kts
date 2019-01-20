49991d095f23dc12face20107dee23354e8bd948
Louis CAD <louis.cognault@gmail.com>
2019-01-20 02:30:25 +0100

    Allow resuming from last step in releasing script
    Closes #159

--- Releasing.kts
+++ Releasing.kts
@@ -17,6 +17,7 @@
 import java.io.File
 import java.util.concurrent.TimeUnit
 import java.net.URI
+import Releasing.BintrayReleaseStep.*
 import java.util.concurrent.CancellationException
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -69,8 +70,51 @@ fun String.executeAndPrint(workingDir: File = dir) {
     }
 }
 
+enum class AnsiColor(private val colorNumber: Byte) {
+    BLACK(0), RED(1), GREEN(2), YELLOW(3), BLUE(4), MAGENTA(5), CYAN(6), WHITE(7);
+
+    companion object {
+        private const val prefix = "\u001B"
+        const val RESET = "$prefix[0m"
+        private val isCompatible = "win" !in System.getProperty("os.name").toLowerCase()
+    }
+
+    val regular get() = if (isCompatible) "$prefix[0;3${colorNumber}m" else ""
+    val bold get() = if (isCompatible) "$prefix[1;3${colorNumber}m" else ""
+    val underline get() = if (isCompatible) "$prefix[4;3${colorNumber}m" else ""
+    val background get() = if (isCompatible) "$prefix[4${colorNumber}m" else ""
+    val highIntensity get() = if (isCompatible) "$prefix[0;9${colorNumber}m" else ""
+    val boldHighIntensity get() = if (isCompatible) "$prefix[1;9${colorNumber}m" else ""
+    val backgroundHighIntensity get() = if (isCompatible) "$prefix[0;10${colorNumber}m" else ""
+}
+
+fun printInfo(message: String) {
+    print(AnsiColor.WHITE.boldHighIntensity)
+    print(AnsiColor.GREEN.background)
+    print(message)
+    println(AnsiColor.RESET)
+}
+
+fun printQuestion(message: String) {
+    print(AnsiColor.WHITE.boldHighIntensity)
+    print(AnsiColor.BLUE.background)
+    print(message)
+    println(AnsiColor.RESET)
+}
+
+fun requestManualAction(instructions: String) {
+    print(AnsiColor.WHITE.boldHighIntensity)
+    print(AnsiColor.BLUE.background)
+    print(instructions)
+    println(AnsiColor.RESET)
+    requestUserConfirmation("Done?")
+}
+
 fun requestUserConfirmation(yesNoQuestion: String) {
-    println("$yesNoQuestion Y/n")
+    print(AnsiColor.WHITE.boldHighIntensity)
+    print(AnsiColor.BLUE.background)
+    print("$yesNoQuestion Y/n")
+    println(AnsiColor.RESET)
     readLine()?.trimEnd().let { input ->
         if (input != "Y" && !"yes".equals(input, ignoreCase = true)) {
             println("Process aborted."); throw CancellationException()
@@ -78,9 +122,83 @@ fun requestUserConfirmation(yesNoQuestion: String) {
     }
 }
 
-fun releasingNonSnapshot() {
+enum class BintrayReleaseStep { // Order of the steps, must be kept right.
+    CHANGE_THIS_LIBRARY_CONSTANT,
+    REQUEST_README_UPDATE_CONFIRMATION,
+    REQUEST_CHANGELOG_UPDATE_CONFIRMATION,
+    COMMIT_PREPARE_FOR_RELEASE_AND_TAG,
+    CLEAN_AND_UPLOAD,
+    PUSH_RELEASE_TO_ORIGIN,
+    REQUEST_PR_SUBMISSION,
+    REQUEST_BINTRAY_PUBLISH,
+    PUSH_TAGS_TO_ORIGIN,
+    REQUEST_PR_MERGE,
+    REQUEST_GITHUB_RELEASE_PUBLICATION,
+    UPDATE_MASTER_BRANCH,
+    UPDATE_DEVELOP_BRANCH_FROM_MASTER,
+    CHANGE_THIS_LIBRARY_CONSTANT_BACK_TO_A_SNAPSHOT,
+    COMMIT_PREPARE_NEXT_DEV_VERSION,
+    PUSH_AT_LAST;
+}
+
+fun checkOnDevelopBranch() {
     val currentBranch = "git rev-parse --abbrev-ref HEAD".execute().trimEnd()
     check(currentBranch == "develop") { "Please, checkout the `develop` branch first." }
+}
+
+val currentSnapshotVersion: String
+val newVersion: String
+val startAtStep: BintrayReleaseStep
+
+val ongoingReleaseFile = dir.resolve("ongoing_release.splitties")
+val versionsFile = dir.resolve("buildSrc/src/main/kotlin/ProjectVersions.kt")
+val libVersionLineStart = "    const val thisLibrary = \""
+
+if (ongoingReleaseFile.exists()) {
+    ongoingReleaseFile.readLines().let {
+        currentSnapshotVersion = it[0]
+        newVersion = it[1]
+        startAtStep = BintrayReleaseStep.valueOf(it[2])
+    }
+} else {
+    checkOnDevelopBranch()
+    currentSnapshotVersion = let { _ ->
+        val libraryVersionLine = versionsFile.readLines().singleOrNull { line ->
+            line.startsWith(libVersionLineStart)
+        } ?: throw IllegalStateException("Library version line not found.")
+        libraryVersionLine.substring(
+            startIndex = libVersionLineStart.length,
+            endIndex = libraryVersionLine.lastIndex
+        ).also { versionName ->
+            check(versionName.endsWith("-SNAPSHOT")) {
+                "Version in ${versionsFile.path} should be a `-SNAPSHOT` version."
+            }
+        }
+    }
+    printInfo("Current version: $currentSnapshotVersion")
+    printQuestion("Please enter the name of the new version you want to release:")
+    newVersion = readLine()?.trimEnd().also { input ->
+        when {
+            input.isNullOrEmpty() -> throw IllegalStateException("No version entered.")
+            input.any { it == ' ' } -> throw IllegalStateException("Versions can't contain spaces.")
+            input.startsWith('v') -> throw IllegalStateException("Please, don't include v prefix.")
+            !input.first().isDigit() -> throw IllegalStateException("Should start with a digit.")
+            !input.all {
+                it.isLetterOrDigit() || it == '.' || it == '-'
+            } -> throw IllegalStateException("Only digits, letters, dots and dashes are allowed.")
+            input.contains("-SNAPSHOT") -> throw IllegalStateException("Snapshots not allowed")
+        }
+        val existingVersions = "git tag".execute().trimEnd().lineSequence().filter {
+            it.startsWith("v") && it.getOrElse(1) { ' ' }.isDigit()
+        }.sorted().toList()
+        check("v$input" !in existingVersions) { "This version already exists!" }
+    }!!
+    startAtStep = CHANGE_THIS_LIBRARY_CONSTANT
+}
+
+@Deprecated("Old version", ReplaceWith("releaseOnBintray()"))
+fun releasingNonSnapshot() {
+    checkOnDevelopBranch()
     val versionsFile = dir.resolve("buildSrc/src/main/kotlin/ProjectVersions.kt")
     val libVersionLineStart = "    const val thisLibrary = \""
     val libraryVersionLine = versionsFile.readLines().singleOrNull { line ->
@@ -141,8 +259,8 @@ fun releasingNonSnapshot() {
     requestUserConfirmation("Done?")
     val pushToOriginWithTagsCommand = "git push origin --tags"
     println("Will now run $pushToOriginWithTagsCommand")
-    pushToOriginWithTagsCommand.executeAndPrint()
     requestUserConfirmation("Continue?")
+    pushToOriginWithTagsCommand.executeAndPrint()
     println("Merge the pull request for the new version on GitHub.")
     requestUserConfirmation("Done?")
     println("Publish release on GitHub.")
@@ -177,6 +295,118 @@ fun releasingNonSnapshot() {
     println("All Done! Let's brag about this new release!!")
 }
 
+fun runBintrayReleaseStep(step: BintrayReleaseStep) = when (step) {
+    CHANGE_THIS_LIBRARY_CONSTANT -> {
+        checkOnDevelopBranch()
+        printInfo("New version: \"$newVersion\"")
+        requestUserConfirmation("Confirm?")
+        val versionsFileTextBeforeEdits = versionsFile.readText()
+        val libraryVersionLine = versionsFile.readLines().singleOrNull { line ->
+            line.startsWith(libVersionLineStart)
+        } ?: throw IllegalStateException("Library version line not found.")
+        versionsFile.writeText(
+            versionsFileTextBeforeEdits.replace(
+                oldValue = libraryVersionLine,
+                newValue = "$libVersionLineStart$newVersion\""
+            )
+        )
+    }
+    REQUEST_README_UPDATE_CONFIRMATION -> {
+        requestManualAction("Update the `README.md` with the new version and any other changes.")
+    }
+    REQUEST_CHANGELOG_UPDATE_CONFIRMATION -> {
+        requestManualAction("Update the `CHANGELOG.md` for the impending release.")
+    }
+    COMMIT_PREPARE_FOR_RELEASE_AND_TAG -> {
+        "git commit -am \"Prepare for release $newVersion\"".executeAndPrint()
+        "git tag -a v$newVersion -m \"Version $newVersion\"".executeAndPrint()
+    }
+    CLEAN_AND_UPLOAD -> {
+        val cleanAndUploadCommand = "./gradlew clean bintrayUpload"
+        printInfo("Running `$cleanAndUploadCommand`")
+        cleanAndUploadCommand.executeAndPrint()
+        printQuestion("Please check upload succeded.")
+    }
+    PUSH_RELEASE_TO_ORIGIN -> {
+        val pushToOriginCommand = "git push origin"
+        printInfo("Will now run $pushToOriginCommand")
+        requestUserConfirmation("Continue?")
+        pushToOriginCommand.executeAndPrint()
+    }
+    REQUEST_PR_SUBMISSION -> {
+        requestManualAction("Create a pull request from the `develop` to the `master` branch on GitHub for the new version, if not already done.")
+    }
+    REQUEST_BINTRAY_PUBLISH -> {
+        requestManualAction("Sign in on Bintray and publish the packages.")
+    }
+    PUSH_TAGS_TO_ORIGIN -> {
+        val pushToOriginWithTagsCommand = "git push origin --tags"
+        printInfo("Will now run $pushToOriginWithTagsCommand")
+        requestUserConfirmation("Continue?")
+        pushToOriginWithTagsCommand.executeAndPrint()
+    }
+    REQUEST_PR_MERGE -> {
+        requestManualAction("Merge the pull request for the new version on GitHub.")
+    }
+    REQUEST_GITHUB_RELEASE_PUBLICATION -> {
+        requestManualAction("Publish release on GitHub.")
+    }
+    UPDATE_MASTER_BRANCH -> {
+        printInfo("Will now checkout the `master` branch, pull from GitHub (origin) to update the local `master` branch.")
+        requestUserConfirmation("Continue?")
+        "git checkout master".executeAndPrint()
+        "git pull origin".executeAndPrint()
+    }
+    UPDATE_DEVELOP_BRANCH_FROM_MASTER -> {
+        printInfo("About to checkout the develop branch (and update it from master for merge commits).")
+        requestUserConfirmation("Continue?")
+        "git checkout develop".executeAndPrint()
+        "git merge master".executeAndPrint()
+    }
+    CHANGE_THIS_LIBRARY_CONSTANT_BACK_TO_A_SNAPSHOT -> {
+        printInfo("Let's update the library for next development version.")
+        printInfo("If you want to keep using $currentSnapshotVersion, enter an empty line.")
+        printInfo("Otherwise, enter the name of the next target version (`-SNAPSHOT` will be added automatically)")
+        val nextDevVersion: String = readLine().let { input ->
+            if (input.isNullOrBlank()) currentSnapshotVersion else "$input-SNAPSHOT"
+        }
+        versionsFile.writeText(
+            versionsFile.readText().replace(
+                oldValue = "$libVersionLineStart$newVersion\"",
+                newValue = "$libVersionLineStart$nextDevVersion\""
+            )
+        )
+        printInfo("${versionsFile.path} has been edited with next developement version ($nextDevVersion).")
+    }
+    COMMIT_PREPARE_NEXT_DEV_VERSION -> {
+        val nextDevVersionCommitCommand = "git commit -am \"Prepare next development version.\""
+        requestUserConfirmation("Will run $nextDevVersionCommitCommand Continue?")
+        nextDevVersionCommitCommand.executeAndPrint()
+    }
+    PUSH_AT_LAST -> {
+        val pushToOriginCommand = "git push origin"
+        requestUserConfirmation("Finally the last step: Running: `$pushToOriginCommand`. Continue?")
+        pushToOriginCommand.executeAndPrint()
+    }
+}
+
+fun releaseOnBintray() {
+    var stepIndex = startAtStep.ordinal
+    val enumValues = enumValues<BintrayReleaseStep>().toList()
+    while (stepIndex < enumValues.size) {
+        val step = enumValues[stepIndex]
+        ongoingReleaseFile.writeText(buildString {
+            appendln(currentSnapshotVersion)
+            appendln(newVersion)
+            appendln(step.name)
+        })
+        runBintrayReleaseStep(step)
+        stepIndex++
+    }
+    ongoingReleaseFile.delete()
+    printQuestion("All Done! Let's brag about this new release!!")
+}
+
 fun openUrl(url: String) {
     val osName = System.getProperty("os.name").toLowerCase()
     val isMacOs: Boolean = "mac" in osName
@@ -188,6 +418,6 @@ fun openUrl(url: String) {
     }
     command.execute()
 }
-releasingNonSnapshot()
+releaseOnBintray()
 val createAndroidStudioCommandLineLauncherUrl = "https://stackoverflow.com/a/48266060/4433326"
 //openUrl(createAndroidStudioCommandLineLauncherUrl)
