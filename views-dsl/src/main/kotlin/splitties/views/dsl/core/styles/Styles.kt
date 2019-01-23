2243b2f9f4fc5cf7d8e88cd784e1c79b9f69caea
Louis CAD <louis.cognault@gmail.com>
2019-01-23 22:53:17 +0100

    Ensure AppCompatStyles are using AppCompat widgets

--- views-dsl/src/main/kotlin/splitties/views/dsl/core/styles/Styles.kt
+++ views-dsl/src/main/kotlin/splitties/views/dsl/core/styles/Styles.kt
@@ -18,6 +18,7 @@ package splitties.views.dsl.core.styles
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.AttrRes
 import androidx.annotation.IdRes
 import androidx.annotation.StyleRes
 import splitties.views.dsl.core.NO_THEME
@@ -25,7 +26,6 @@ import splitties.views.dsl.core.getThemeAttrStyledView
 import splitties.views.dsl.core.viewFactory
 import splitties.views.dsl.core.wrapCtxIfNeeded
 
-@Deprecated("No longer used. Will be removed before Splitties 3.0.0.")
 typealias NewViewWithStyleAttrRef<V> = (Context, AttributeSet?, Int) -> V
 
 inline operator fun <reified V : View> XmlStyle<V>.invoke(
@@ -34,5 +34,15 @@ inline operator fun <reified V : View> XmlStyle<V>.invoke(
     @StyleRes theme: Int = NO_THEME,
     initView: V.() -> Unit = {}
 ): V = ctx.viewFactory.getThemeAttrStyledView<V>(ctx.wrapCtxIfNeeded(theme), null, styleAttr).also {
+    it.id = id
+}.apply(initView)
+
+inline fun <V : View> Context.styledView(
+    newViewRef: NewViewWithStyleAttrRef<V>,
+    @AttrRes styleAttr: Int,
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: V.() -> Unit = {}
+): V = newViewRef(this.wrapCtxIfNeeded(theme), null, styleAttr).also {
     it.id = id
 }.apply(initView)
