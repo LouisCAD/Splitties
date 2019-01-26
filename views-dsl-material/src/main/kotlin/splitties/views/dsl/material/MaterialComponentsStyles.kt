3a1959f3fae5b17dd4c112320661b90cde9d2cbe
Louis CAD <louis.cognault@gmail.com>
2019-01-26 11:24:13 +0100

    Link official MaterialButton documentation

--- views-dsl-material/src/main/kotlin/splitties/views/dsl/material/MaterialComponentsStyles.kt
+++ views-dsl-material/src/main/kotlin/splitties/views/dsl/material/MaterialComponentsStyles.kt
@@ -13,6 +13,10 @@ class MaterialComponentsStyles(@PublishedApi internal val ctx: Context) {
         ctx.theme.applyStyle(R.style.MaterialComponentsStyles, false)
     }
 
+    /**
+     * See [MaterialButton] documentation on [GitHub](https://github.com/material-components/material-components-android/blob/master/docs/components/MaterialButton.md)
+     * and on [d.android.com website](https://developer.android.com/reference/com/google/android/material/button/MaterialButton).
+     */
     inline val button get() = ButtonMaterialComponentsStyles(ctx)
 }
 
