10a7089617bb39463357d0ae375d692056db0f28
Louis CAD <louis.cognault@gmail.com>
2019-02-03 11:29:51 +0100

    Add BottomAppBarStyles

--- views-dsl-material/src/main/kotlin/splitties/views/dsl/material/MaterialComponentsStyles.kt
+++ views-dsl-material/src/main/kotlin/splitties/views/dsl/material/MaterialComponentsStyles.kt
@@ -1,13 +1,21 @@
 package splitties.views.dsl.material
 
 import android.content.Context
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.button.MaterialButton
+import splitties.views.dsl.material.styles.BottomAppBarStyles
 import splitties.views.dsl.material.styles.ButtonMaterialComponentsStyles
 
 class MaterialComponentsStyles(@PublishedApi internal val ctx: Context) {
     init {
         ctx.theme.applyStyle(R.style.MaterialComponentsStyles, false)
     }
+
+    /**
+     * See [BottomAppBar] documentation on [GitHub](https://github.com/material-components/material-components-android/blob/master/docs/components/BottomAppBar.md)
+     * and on [d.android.com website](https://developer.android.com/reference/com/google/android/material/bottomappbar/BottomAppBar).
+     */
+    inline val bottomAppBar get() = BottomAppBarStyles(ctx)
 
     /**
      * See [MaterialButton] documentation on [GitHub](https://github.com/material-components/material-components-android/blob/master/docs/components/MaterialButton.md)
