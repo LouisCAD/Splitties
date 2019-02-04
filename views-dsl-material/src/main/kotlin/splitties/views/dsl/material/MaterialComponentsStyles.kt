f56e5fd52a22d3b895e35b3fdbc31464a62ee61c
Louis CAD <louis.cognault@gmail.com>
2019-02-04 03:09:59 +0100

    Add ChipStyles

--- views-dsl-material/src/main/kotlin/splitties/views/dsl/material/MaterialComponentsStyles.kt
+++ views-dsl-material/src/main/kotlin/splitties/views/dsl/material/MaterialComponentsStyles.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.chip.Chip
 import splitties.views.dsl.material.styles.BottomAppBarStyles
 import splitties.views.dsl.material.styles.BottomNavigationViewStyles
 import splitties.views.dsl.material.styles.ButtonMaterialComponentsStyles
+import splitties.views.dsl.material.styles.ChipStyles
 
 class MaterialComponentsStyles(@PublishedApi internal val ctx: Context) {
     init {
@@ -30,4 +32,10 @@ class MaterialComponentsStyles(@PublishedApi internal val ctx: Context) {
      * and on [d.android.com website](https://developer.android.com/reference/com/google/android/material/button/MaterialButton).
      */
     inline val button get() = ButtonMaterialComponentsStyles(ctx)
+
+    /**
+     * See [Chip] documentation on [GitHub](https://github.com/material-components/material-components-android/blob/master/docs/components/Chip.md)
+     * and on [d.android.com website](https://developer.android.com/reference/com/google/android/material/chip/Chip).
+     */
+    inline val chip get() = ChipStyles(ctx)
 }
