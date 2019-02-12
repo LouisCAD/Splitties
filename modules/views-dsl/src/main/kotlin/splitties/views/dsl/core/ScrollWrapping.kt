ff4ce95f42e99c9e835486667cba456fbb87ede1
Louis CAD <louis.cognault@gmail.com>
2019-02-12 23:08:43 +0100

    Add wrapInScrollView and wrapInHorizontalScrollView
    Resolves #171

---
+++ modules/views-dsl/src/main/kotlin/splitties/views/dsl/core/ScrollWrapping.kt
@@ -0,0 +1,30 @@
+package splitties.views.dsl.core
+
+import android.view.View
+import android.widget.HorizontalScrollView
+import android.widget.ScrollView
+import androidx.annotation.IdRes
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+inline fun View.wrapInScrollView(
+    @IdRes id: Int = View.NO_ID,
+    height: Int = wrapContent,
+    initView: ScrollView.() -> Unit = {}
+): ScrollView {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return view(::ScrollView, id) {
+        add(this@wrapInScrollView, lParams(width = matchParent, height = height))
+    }.apply(initView)
+}
+
+inline fun View.wrapInHorizontalScrollView(
+    @IdRes id: Int = View.NO_ID,
+    height: Int = wrapContent,
+    initView: HorizontalScrollView.() -> Unit = {}
+): HorizontalScrollView {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return view(::HorizontalScrollView, id) {
+        add(this@wrapInHorizontalScrollView, lParams(width = matchParent, height = height))
+    }.apply(initView)
+}
