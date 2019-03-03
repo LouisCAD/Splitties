73a028d708ca0aeb10ec712d7a6b309f514bbc40
Louis CAD <louis.cognault@gmail.com>
2019-03-03 12:03:55 +0100

    Prepare next development version.

--- buildSrc/src/main/kotlin/ProjectVersions.kt
+++ buildSrc/src/main/kotlin/ProjectVersions.kt
@@ -5,7 +5,7 @@
 object ProjectVersions {
     const val androidBuildTools = "28.0.3"
     const val androidSdk = 28
-    const val thisLibrary = "3.0.0-alpha04"
+    const val thisLibrary = "3.0.0-dev-001"
 }
 
 val isDevVersion = ProjectVersions.thisLibrary.contains("-dev-")
