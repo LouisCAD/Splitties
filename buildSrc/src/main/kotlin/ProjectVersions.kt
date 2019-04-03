23672dc367e2acd17fceea066d458b5908229cae
Louis CAD <louis.cognault@gmail.com>
2019-04-03 12:58:07 +0200

    Dev version 3.0.0-dev-004

--- buildSrc/src/main/kotlin/ProjectVersions.kt
+++ buildSrc/src/main/kotlin/ProjectVersions.kt
@@ -5,7 +5,7 @@
 object ProjectVersions {
     const val androidBuildTools = "28.0.3"
     const val androidSdk = 28
-    const val thisLibrary = "3.0.0-dev-003"
+    const val thisLibrary = "3.0.0-dev-004"
 }
 
 val isDevVersion = ProjectVersions.thisLibrary.contains("-dev-")
