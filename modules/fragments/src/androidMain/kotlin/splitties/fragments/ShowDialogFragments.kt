4480edb2d871926299392fd4c8740c353c16de3b
Louis CAD <louis.cognault@gmail.com>
2019-04-07 18:20:05 +0200

    Add documentation to functions in ShowDialogFragments.kt
    This commit also adds an optional commitNowWhenResumed parameter to
    the show function that defaults to true.

--- modules/fragments/src/androidMain/kotlin/splitties/fragments/ShowDialogFragments.kt
+++ modules/fragments/src/androidMain/kotlin/splitties/fragments/ShowDialogFragments.kt
@@ -2,8 +2,6 @@
  * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
  */
 
-@file:UseExperimental(PotentialFutureAndroidXLifecycleKtxApi::class)
-
 package splitties.fragments
 
 import androidx.fragment.app.DialogFragment
@@ -18,53 +16,124 @@ import splitties.lifecycle.coroutines.PotentialFutureAndroidXLifecycleKtxApi
 import splitties.lifecycle.coroutines.awaitState
 import splitties.lifecycle.coroutines.coroutineScope
 
+/**
+ * Shows the [DialogFragment] created in [newDialogRef] as soon as [lifecycle] is in
+ * [Lifecycle.State.RESUMED], immediately if possible. If the lifecycle is not in the resumed state
+ * when this function is called and it reaches [Lifecycle.State.DESTROYED] before being resumed,
+ * the [DialogFragment] will not be shown.
+ *
+ * You can specify the [tag] for later retrieval in [FragmentManager.findFragmentByTag], and
+ * you can put arguments in the optional [setup] inline lambda.
+ */
 @ExperimentalSplittiesApi
 inline fun <DF : DialogFragment> FragmentManager.showAsync(
     lifecycle: Lifecycle,
     newDialogRef: () -> DF,
     tag: String? = null,
     setup: DF.() -> Unit = {}
-) = newDialogRef().apply(setup).also {
+): DF = newDialogRef().apply(setup).also {
+    @UseExperimental(PotentialFutureAndroidXLifecycleKtxApi::class)
     if (isStateSaved) lifecycle.coroutineScope.launch {
         lifecycle.awaitState(RESUMED)
         it.show(this@showAsync, tag)
     } else it.show(this, tag)
 }
 
+/**
+ * Shows the [DialogFragment] created in [newDialogRef] as soon as this [Fragment]'s lifecycle is in
+ * [Lifecycle.State.RESUMED], immediately if possible. If the lifecycle is not in the resumed state
+ * when this function is called and it reaches [Lifecycle.State.DESTROYED] before being resumed,
+ * the [DialogFragment] will not be shown.
+ *
+ * You can specify the [tag] for later retrieval in [FragmentManager.findFragmentByTag], and
+ * you can put arguments in the optional [setup] inline lambda.
+ */
 @ExperimentalSplittiesApi
 inline fun <DF : DialogFragment> Fragment.showAsync(
     newDialogRef: () -> DF,
     tag: String? = null,
     setup: DF.() -> Unit = {}
-) = fragmentManager!!.showAsync(lifecycle, newDialogRef, tag, setup)
+): DF = fragmentManager!!.showAsync(lifecycle, newDialogRef, tag, setup)
 
+/**
+ * Shows the [DialogFragment] created in [newDialogRef] as soon as this Activity's lifecycle is in
+ * [Lifecycle.State.RESUMED], immediately if possible. If the lifecycle is not in the resumed state
+ * when this function is called and it reaches [Lifecycle.State.DESTROYED] before being resumed,
+ * the [DialogFragment] will not be shown.
+ *
+ * You can specify the [tag] for later retrieval in [FragmentManager.findFragmentByTag], and
+ * you can put arguments in the optional [setup] inline lambda.
+ */
 @ExperimentalSplittiesApi
 inline fun <DF : DialogFragment> FragmentActivity.showAsync(
     newDialogRef: () -> DF,
     tag: String? = null,
     setup: DF.() -> Unit = {}
-) = supportFragmentManager.showAsync(lifecycle, newDialogRef, tag, setup)
+): DF = supportFragmentManager.showAsync(lifecycle, newDialogRef, tag, setup)
 
+/**
+ * Shows the [DialogFragment] created in [newDialogRef] as soon as [lifecycle] is in
+ * [Lifecycle.State.RESUMED], immediately if possible. If the lifecycle is not in the resumed state
+ * when this function is called and it reaches [Lifecycle.State.DESTROYED] before being resumed,
+ * the [DialogFragment] will not be shown.
+ *
+ * You can specify the [tag] for later retrieval in [FragmentManager.findFragmentByTag], and
+ * you can put arguments in the optional [setup] inline lambda.
+ *
+ * This functions resumes only after the [DialogFragment] has been shown, unless you set
+ * [commitNowWhenResumed] to false, in which case it'll be shown after [FragmentManager] executes
+ * its pending transactions.
+ */
 suspend inline fun <DF : DialogFragment> FragmentManager.show(
     lifecycle: Lifecycle,
     newDialogRef: () -> DF,
     tag: String? = null,
+    commitNowWhenResumed: Boolean = true,
     setup: DF.() -> Unit = {}
-) = newDialogRef().apply(setup).also {
+): DF = newDialogRef().apply(setup).also {
     if (isStateSaved) {
+        @UseExperimental(PotentialFutureAndroidXLifecycleKtxApi::class)
         lifecycle.awaitState(RESUMED)
-        it.show(this, tag)
-    } else it.show(this, tag)
+        if (commitNowWhenResumed) it.showNow(this, tag) else it.show(this, tag)
+    } else if (commitNowWhenResumed) it.showNow(this, tag) else it.show(this, tag)
 }
 
+/**
+ * Shows the [DialogFragment] created in [newDialogRef] as soon as this [Fragment]'s lifecycle is in
+ * [Lifecycle.State.RESUMED], immediately if possible. If the lifecycle is not in the resumed state
+ * when this function is called and it reaches [Lifecycle.State.DESTROYED] before being resumed,
+ * the [DialogFragment] will not be shown.
+ *
+ * You can specify the [tag] for later retrieval in [FragmentManager.findFragmentByTag], and
+ * you can put arguments in the optional [setup] inline lambda.
+ *
+ * This functions resumes only after the [DialogFragment] has been shown, unless you set
+ * [commitNowWhenResumed] to false, in which case it'll be shown after [FragmentManager] executes
+ * its pending transactions.
+ */
 suspend inline fun <DF : DialogFragment> Fragment.show(
     newDialogRef: () -> DF,
     tag: String? = null,
+    commitNowWhenResumed: Boolean = true,
     setup: DF.() -> Unit = {}
-) = fragmentManager!!.show(lifecycle, newDialogRef, tag, setup)
+): DF = fragmentManager!!.show(lifecycle, newDialogRef, tag, commitNowWhenResumed, setup)
 
+/**
+ * Shows the [DialogFragment] created in [newDialogRef] as soon as this Activity's lifecycle is in
+ * [Lifecycle.State.RESUMED], immediately if possible. If the lifecycle is not in the resumed state
+ * when this function is called and it reaches [Lifecycle.State.DESTROYED] before being resumed,
+ * the [DialogFragment] will not be shown.
+ *
+ * You can specify the [tag] for later retrieval in [FragmentManager.findFragmentByTag], and
+ * you can put arguments in the optional [setup] inline lambda.
+ *
+ * This functions resumes only after the [DialogFragment] has been shown, unless you set
+ * [commitNowWhenResumed] to false, in which case it'll be shown after [FragmentManager] executes
+ * its pending transactions.
+ */
 suspend inline fun <DF : DialogFragment> FragmentActivity.show(
     newDialogRef: () -> DF,
     tag: String? = null,
+    commitNowWhenResumed: Boolean = true,
     setup: DF.() -> Unit = {}
-) = supportFragmentManager.show(lifecycle, newDialogRef, tag, setup)
+): DF = supportFragmentManager.show(lifecycle, newDialogRef, tag, commitNowWhenResumed, setup)
