ad5f7e79e98b112e919c3f6b83515391bbcb8ee4
Louis CAD <louis.cognault@gmail.com>
2019-04-26 22:14:20 +0200

    AGP 3.5.0-alpha13

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,7 @@ val kotlinVersion = "1.3.30" // Don't forget to update in Dependencies.kt too!
 
 dependencies {
     compileOnly(gradleApi())
-    implementation("com.android.tools.build:gradle:3.5.0-alpha11")
+    implementation("com.android.tools.build:gradle:3.5.0-alpha13")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
 }
