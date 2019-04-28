09c724e3d1fc59ea79e3301bf727207acb35565d
Louis CAD <louis.cognault@gmail.com>
2019-04-28 19:34:40 +0200

    Add documentation to hasPermission function

--- modules/permissions/src/androidMain/kotlin/splitties/permissions/SuspendPermissionRequest.kt
+++ modules/permissions/src/androidMain/kotlin/splitties/permissions/SuspendPermissionRequest.kt
@@ -16,6 +16,11 @@ import splitties.fragments.show
 import splitties.init.appCtx
 import splitties.lifecycle.coroutines.PotentialFutureAndroidXLifecycleKtxApi
 
+/**
+ * Returns true if the passed [permission] is granted, or if the device API level is lower than 23
+ * (runtime permissions were introduced in API level 23, and are granted at app install on older
+ * Android API levels).
+ */
 @ExperimentalSplittiesApi
 fun hasPermission(permission: String): Boolean = SDK_INT < 23 ||
         appCtx.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
