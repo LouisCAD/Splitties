5032dfe0487e0025cdf259a89422ef72081259a7
Louis CAD <louis.cognault@gmail.com>
2019-05-05 13:15:49 +0200

    Fix dev version name

--- buildSrc/src/main/kotlin/ProjectVersions.kt
+++ buildSrc/src/main/kotlin/ProjectVersions.kt
@@ -5,7 +5,7 @@
 object ProjectVersions {
     const val androidBuildTools = "28.0.3"
     const val androidSdk = 28
-    const val thisLibrary = "3.0.0-dev-010-dev-001"
+    const val thisLibrary = "3.0.0-dev-010"
 }
 
 val isDevVersion = ProjectVersions.thisLibrary.contains("-dev-")
