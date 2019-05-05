a8a31b0afb0b2c91c839f4f9532440d2fc087a79
Louis CAD <louis.cognault@gmail.com>
2019-05-05 13:13:36 +0200

    Make Repositories.kt more readable

--- buildSrc/src/main/kotlin/Repositories.kt
+++ buildSrc/src/main/kotlin/Repositories.kt
@@ -4,49 +4,31 @@
 
 @file:Suppress("UnstableApiUsage")
 
-import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository as MAR
 
-fun MavenArtifactRepository.ensureGroups(vararg group: String): MavenArtifactRepository = apply {
+fun MAR.ensureGroups(vararg group: String): MAR = apply {
+    content { group.forEach { includeGroup(it) } }
+}
+
+fun MAR.ensureGroupsByRegexp(vararg regexp: String): MAR = apply {
+    content { regexp.forEach { includeGroupByRegex(it) } }
+}
+
+fun MAR.ensureGroupsStartingWith(vararg regexp: String): MAR = apply {
+    content { regexp.forEach { includeGroupByRegex(it.replace(".", "\\.") + ".*") } }
+}
+
+fun MAR.ensureModules(vararg modules: String): MAR = apply {
+    content { modules.forEach { includeModule(it.substringBefore(':'), it.substringAfter(':')) } }
+}
+
+fun MAR.ensureModulesByRegexp(vararg regexp: String): MAR = apply {
     content {
-        group.forEach {
-            includeGroup(it)
-        }
+        regexp.forEach { includeModuleByRegex(it.substringBefore(':'), it.substringAfter(':')) }
     }
 }
 
-fun MavenArtifactRepository.ensureGroupsByRegexp(vararg regexp: String): MavenArtifactRepository = apply {
-    content {
-        regexp.forEach {
-            includeGroupByRegex(it)
-        }
-    }
-}
-
-fun MavenArtifactRepository.ensureGroupsStartingWith(vararg regexp: String): MavenArtifactRepository = apply {
-    content {
-        regexp.forEach {
-            includeGroupByRegex(it.replace(".", "\\.") + ".*")
-        }
-    }
-}
-
-fun MavenArtifactRepository.ensureModules(vararg modules: String): MavenArtifactRepository = apply {
-    content {
-        modules.forEach {
-            includeModule(it.substringBefore(':'), it.substringAfter(':'))
-        }
-    }
-}
-
-fun MavenArtifactRepository.ensureModulesByRegexp(vararg regexp: String): MavenArtifactRepository = apply {
-    content {
-        regexp.forEach {
-            includeModuleByRegex(it.substringBefore(':'), it.substringAfter(':'))
-        }
-    }
-}
-
-fun MavenArtifactRepository.ensureModulesStartingWith(vararg regexp: String): MavenArtifactRepository = apply {
+fun MAR.ensureModulesStartingWith(vararg regexp: String): MAR = apply {
     content {
         regexp.forEach {
             val groupRegex = it.substringBefore(':').replace(".", "\\.")
