509a9e01f88b752aef6d3e214c12cce802d7d03d
Louis CAD <louis.cognault@gmail.com>
2019-06-02 15:54:32 +0200

    Deprecate usage of property name for pref key

--- modules/preferences/src/androidMain/kotlin/splitties/preferences/PrefDelegate.kt
+++ modules/preferences/src/androidMain/kotlin/splitties/preferences/PrefDelegate.kt
@@ -2,6 +2,8 @@
  * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
  */
 
+@file:Suppress("NOTHING_TO_INLINE")
+
 package splitties.preferences
 
 import android.content.SharedPreferences
@@ -18,8 +20,8 @@ import kotlin.reflect.KProperty
  * This class doesn't extend [ReadWriteProperty] to avoid double autoboxing on primitive values.
  */
 sealed class PrefDelegate<T>(
-    private val preferences: Preferences,
-    val key: String?
+    @JvmField protected val preferences: Preferences,
+    @JvmField val key: String
 ) {
     @ExperimentalSplittiesApi
     fun doesBelongTo(sharedPreferences: SharedPreferences): Boolean {
@@ -29,183 +31,187 @@ sealed class PrefDelegate<T>(
 
     @FlowPreview
     @ExperimentalSplittiesApi
-    fun changesFlow(): Flow<Unit> {
-        if (key == null) throw UnsupportedOperationException(
-            "To use changesFlow, you need to " +
-                    "specify the key of the preference item explicitly."
-        )
-        return flowViaChannel(Channel.CONFLATED) { sendChannel ->
-            val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, changedKey ->
-                if (key == changedKey) runCatching { sendChannel.offer(Unit) }
-            }
-            preferences.prefs.registerOnSharedPreferenceChangeListener(listener)
-            @Suppress("EXPERIMENTAL_API_USAGE")
-            sendChannel.invokeOnClose {
-                preferences.prefs.unregisterOnSharedPreferenceChangeListener(listener)
-            }
+    fun changesFlow(): Flow<Unit> = flowViaChannel(Channel.CONFLATED) { sendChannel ->
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, changedKey ->
+            if (key == changedKey) runCatching { sendChannel.offer(Unit) }
+        }
+        preferences.prefs.registerOnSharedPreferenceChangeListener(listener)
+        @Suppress("EXPERIMENTAL_API_USAGE")
+        sendChannel.invokeOnClose {
+            preferences.prefs.unregisterOnSharedPreferenceChangeListener(listener)
         }
     }
 
     @FlowPreview
-    fun valueFlow(): Flow<T> {
-        if (key == null) throw UnsupportedOperationException(
-            "To use valueFlow, you need to " +
-                    "specify the key of the preference item explicitly."
-        )
-        return flowViaChannel<T>(Channel.CONFLATED) { sendChannel ->
-            val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, changedKey ->
-                if (key == changedKey) runCatching {
-                    sendChannel.offer(getValue())
-                }
-            }
-            preferences.prefs.registerOnSharedPreferenceChangeListener(listener)
-            runCatching {
+    fun valueFlow(): Flow<T> = flowViaChannel<T>(Channel.CONFLATED) { sendChannel ->
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, changedKey ->
+            if (key == changedKey) runCatching {
                 sendChannel.offer(getValue())
             }
-            @Suppress("EXPERIMENTAL_API_USAGE")
-            sendChannel.invokeOnClose {
-                preferences.prefs.unregisterOnSharedPreferenceChangeListener(listener)
-            }
-        }.distinctUntilChanged()
-    }
+        }
+        preferences.prefs.registerOnSharedPreferenceChangeListener(listener)
+        runCatching {
+            sendChannel.offer(getValue())
+        }
+        @Suppress("EXPERIMENTAL_API_USAGE")
+        sendChannel.invokeOnClose {
+            preferences.prefs.unregisterOnSharedPreferenceChangeListener(listener)
+        }
+    }.distinctUntilChanged()
 
     @Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
     private fun getValue(): T = when (this) {
-        is BoolPref -> preferences.prefs.getBoolean(key!!, defaultValue)
-        is IntPref -> preferences.prefs.getInt(key!!, defaultValue)
-        is FloatPref -> preferences.prefs.getFloat(key!!, defaultValue)
-        is LongPref -> preferences.prefs.getLong(key!!, defaultValue)
-        is StringPref -> preferences.prefs.getString(key!!, defaultValue)!!
-        is StringOrNullPref -> preferences.prefs.getString(key!!, defaultValue)
-        is StringSetPref -> preferences.prefs.getStringSet(key!!, defaultValue)
-        is StringSetOrNullPref -> preferences.prefs.getStringSet(key!!, defaultValue)!!
+        is BoolPref -> preferences.prefs.getBoolean(key, defaultValue)
+        is IntPref -> preferences.prefs.getInt(key, defaultValue)
+        is FloatPref -> preferences.prefs.getFloat(key, defaultValue)
+        is LongPref -> preferences.prefs.getLong(key, defaultValue)
+        is StringPref -> preferences.prefs.getString(key, defaultValue)!!
+        is StringOrNullPref -> preferences.prefs.getString(key, defaultValue)
+        is StringSetPref -> preferences.prefs.getStringSet(key, defaultValue)
+        is StringSetOrNullPref -> preferences.prefs.getStringSet(key, defaultValue)!!
     } as T
 }
 
 class BoolPref @PublishedApi internal constructor(
-    private val preferences: Preferences,
-    key: String?,
+    preferences: Preferences,
+    key: String,
     val defaultValue: Boolean
 ) : PrefDelegate<Boolean>(preferences, key) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): Boolean {
-        return preferences.prefs.getBoolean(key ?: prop.name, defaultValue)
-    }
 
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: Boolean) {
-        with(preferences) {
-            editor.putBoolean(key ?: prop.name, value).attemptApply()
+    var value: Boolean
+        get() = preferences.prefs.getBoolean(key, defaultValue)
+        set(value) = with(preferences) {
+            editor.putBoolean(key, value).attemptApply()
         }
+
+    inline operator fun getValue(thisRef: Any?, prop: KProperty<*>?): Boolean = value
+    inline operator fun setValue(thisRef: Any?, prop: KProperty<*>?, value: Boolean) {
+        this.value = value
     }
 }
 
 class IntPref @PublishedApi internal constructor(
-    private val preferences: Preferences,
-    key: String?,
+    preferences: Preferences,
+    key: String,
     val defaultValue: Int
 ) : PrefDelegate<Int>(preferences, key) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): Int {
-        return preferences.prefs.getInt(key ?: prop.name, defaultValue)
-    }
 
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: Int) {
-        with(preferences) {
-            editor.putInt(key ?: prop.name, value).attemptApply()
+    var value: Int
+        get() = preferences.prefs.getInt(key, defaultValue)
+        set(value) = with(preferences) {
+            editor.putInt(key, value).attemptApply()
         }
+
+    inline operator fun getValue(thisRef: Any?, prop: KProperty<*>?): Int = value
+    inline operator fun setValue(thisRef: Any?, prop: KProperty<*>?, value: Int) {
+        this.value = value
     }
 }
 
 class FloatPref @PublishedApi internal constructor(
-    private val preferences: Preferences,
-    key: String?,
+    preferences: Preferences,
+    key: String,
     val defaultValue: Float
 ) : PrefDelegate<Float>(preferences, key) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): Float {
-        return preferences.prefs.getFloat(key ?: prop.name, defaultValue)
-    }
 
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: Float) {
-        with(preferences) {
-            editor.putFloat(key ?: prop.name, value).attemptApply()
+    var value: Float
+        get() = preferences.prefs.getFloat(key, defaultValue)
+        set(value) = with(preferences) {
+            editor.putFloat(key, value).attemptApply()
         }
+
+    inline operator fun getValue(thisRef: Any?, prop: KProperty<*>?): Float = value
+    inline operator fun setValue(thisRef: Any?, prop: KProperty<*>?, value: Float) {
+        this.value = value
     }
 }
 
 class LongPref @PublishedApi internal constructor(
-    private val preferences: Preferences,
-    key: String?,
+    preferences: Preferences,
+    key: String,
     val defaultValue: Long
 ) : PrefDelegate<Long>(preferences, key) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): Long {
-        return preferences.prefs.getLong(key ?: prop.name, defaultValue)
-    }
 
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: Long) {
-        with(preferences) {
-            editor.putLong(key ?: prop.name, value).attemptApply()
+    var value: Long
+        get() = preferences.prefs.getLong(key, defaultValue)
+        set(value) = with(preferences) {
+            editor.putLong(key, value).attemptApply()
         }
+
+    inline operator fun getValue(thisRef: Any?, prop: KProperty<*>?): Long = value
+    inline operator fun setValue(thisRef: Any?, prop: KProperty<*>?, value: Long) {
+        this.value = value
     }
 }
 
 class StringPref @PublishedApi internal constructor(
-    private val preferences: Preferences,
-    key: String?,
+    preferences: Preferences,
+    key: String,
     val defaultValue: String
 ) : PrefDelegate<String>(preferences, key) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): String {
-        return preferences.prefs.getString(key ?: prop.name, defaultValue)!!
-    }
 
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: String) {
-        with(preferences) {
-            editor.putString(key ?: prop.name, value).attemptApply()
+    var value: String
+        get() = preferences.prefs.getString(key, defaultValue)!!
+        set(value) = with(preferences) {
+            editor.putString(key, value).attemptApply()
         }
+
+    inline operator fun getValue(thisRef: Any?, prop: KProperty<*>?): String = value
+    inline operator fun setValue(thisRef: Any?, prop: KProperty<*>?, value: String) {
+        this.value = value
     }
 }
 
 class StringOrNullPref @PublishedApi internal constructor(
-    private val preferences: Preferences,
-    key: String?,
+    preferences: Preferences,
+    key: String,
     val defaultValue: String?
 ) : PrefDelegate<String?>(preferences, key) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): String? {
-        return preferences.prefs.getString(key ?: prop.name, defaultValue)
-    }
 
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: String?) {
-        with(preferences) {
-            editor.putString(key ?: prop.name, value).attemptApply()
+    var value: String?
+        get() = preferences.prefs.getString(key, defaultValue)
+        set(value) = with(preferences) {
+            editor.putString(key, value).attemptApply()
         }
+
+    inline operator fun getValue(thisRef: Any?, prop: KProperty<*>?): String? = value
+    inline operator fun setValue(thisRef: Any?, prop: KProperty<*>?, value: String?) {
+        this.value = value
     }
 }
 
 class StringSetPref @PublishedApi internal constructor(
-    private val preferences: Preferences,
-    key: String?,
+    preferences: Preferences,
+    key: String,
     val defaultValue: Set<String>
 ) : PrefDelegate<Set<String>>(preferences, key) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): Set<String> {
-        return preferences.prefs.getStringSet(key ?: prop.name, defaultValue)!!
-    }
 
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: Set<String>) {
-        with(preferences) {
-            editor.putStringSet(key ?: prop.name, value).attemptApply()
+    var value: Set<String>
+        get() = preferences.prefs.getStringSet(key, defaultValue)!!
+        set(value) = with(preferences) {
+            editor.putStringSet(key, value).attemptApply()
         }
+
+    inline operator fun getValue(thisRef: Any?, prop: KProperty<*>?): Set<String> = value
+    inline operator fun setValue(thisRef: Any?, prop: KProperty<*>?, value: Set<String>) {
+        this.value = value
     }
 }
 
 class StringSetOrNullPref @PublishedApi internal constructor(
-    private val preferences: Preferences,
-    key: String?,
+    preferences: Preferences,
+    key: String,
     val defaultValue: Set<String>?
 ) : PrefDelegate<Set<String>?>(preferences, key) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): Set<String>? {
-        return preferences.prefs.getStringSet(key ?: prop.name, defaultValue)
-    }
 
-    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: Set<String>?) {
-        with(preferences) {
-            editor.putStringSet(key ?: prop.name, value).attemptApply()
+    var value: Set<String?>
+        get() = preferences.prefs.getStringSet(key, defaultValue)!!
+        set(value) = with(preferences) {
+            editor.putStringSet(key, value).attemptApply()
         }
+
+    inline operator fun getValue(thisRef: Any?, prop: KProperty<*>?): Set<String?> = value
+    inline operator fun setValue(thisRef: Any?, prop: KProperty<*>?, value: Set<String?>) {
+        this.value = value
     }
 }
--- modules/preferences/src/androidMain/kotlin/splitties/preferences/Preferences.kt
+++ modules/preferences/src/androidMain/kotlin/splitties/preferences/Preferences.kt
@@ -2,13 +2,22 @@
  * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
  */
 
-@file:Suppress("NOTHING_TO_INLINE")
+@file:Suppress(
+    "NOTHING_TO_INLINE", "SameParameterValue", "unused",
+    "DeprecatedCallableAddReplaceWith"
+)
 @file:UseExperimental(InternalSplittiesApi::class)
 
 package splitties.preferences
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build.VERSION.SDK_INT
 import splitties.experimental.InternalSplittiesApi
+import splitties.init.appCtx
+import splitties.init.directBootCtx
+import kotlin.reflect.KProperty
 
 abstract class Preferences(
     name: String,
@@ -108,63 +117,163 @@ abstract class Preferences(
         defaultValue: Set<String>? = null
     ) = StringSetOrNullPref(this, key = key, defaultValue = defaultValue)
 
-    @DangerousRenaming
-    protected inline fun boolPref(defaultValue: Boolean) = BoolPref(
-        this,
-        key = null,
-        defaultValue = defaultValue
-    )
+    @Suppress("DEPRECATION")
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected fun boolPref(defaultValue: Boolean) = BoolPrefProvider(defaultValue)
 
-    @DangerousRenaming
-    protected inline fun intPref(defaultValue: Int) = IntPref(
-        this,
-        key = null,
-        defaultValue = defaultValue
-    )
+    @Suppress("DEPRECATION")
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected fun intPref(defaultValue: Int) = IntPrefProvider(defaultValue)
 
-    @DangerousRenaming
-    protected inline fun floatPref(defaultValue: Float) = FloatPref(
-        this,
-        key = null,
-        defaultValue = defaultValue
-    )
+    @Suppress("DEPRECATION")
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected fun floatPref(defaultValue: Float) = FloatPrefProvider(defaultValue)
 
-    @DangerousRenaming
-    protected inline fun longPref(defaultValue: Long) = LongPref(
-        this,
-        key = null,
-        defaultValue = defaultValue
-    )
+    @Suppress("DEPRECATION")
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected fun longPref(defaultValue: Long) = LongPrefProvider(defaultValue)
 
-    @DangerousRenaming
-    protected inline fun stringPref(defaultValue: String) = StringPref(
-        this,
-        key = null,
-        defaultValue = defaultValue
-    )
+    @Suppress("DEPRECATION")
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected fun stringPref(defaultValue: String) = StringPrefProvider(defaultValue)
 
-    @DangerousRenaming
-    protected inline fun stringOrNullPref(defaultValue: String?) = StringOrNullPref(
-        this,
-        key = null,
-        defaultValue = defaultValue
-    )
+    @Suppress("DEPRECATION")
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected fun stringOrNullPref(
+        defaultValue: String?
+    ) = StringOrNullPrefProvider(defaultValue)
 
-    @DangerousRenaming
-    protected inline fun stringSetPref(defaultValue: Set<String> = emptySet()) = StringSetPref(
-        this,
-        key = null,
-        defaultValue = defaultValue
-    )
+    @Suppress("DEPRECATION")
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected fun stringSetPref(
+        defaultValue: Set<String> = emptySet()
+    ) = StringSetPrefProvider(defaultValue)
 
-    @DangerousRenaming
-    protected inline fun stringSetOrNullPref(
+    @Suppress("DEPRECATION")
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected fun stringSetOrNullPref(
         defaultValue: Set<String>? = null
-    ) = StringSetOrNullPref(
-        this,
-        key = null,
-        defaultValue = defaultValue
-    )
+    ) = StringSetOrNullPrefProvider(defaultValue)
 
-    companion object
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected inner class BoolPrefProvider internal constructor(private val defaultValue: Boolean) {
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): BoolPref {
+            return BoolPref(property.name, defaultValue)
+        }
+    }
+
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected inner class IntPrefProvider internal constructor(private val defaultValue: Int) {
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): IntPref {
+            return IntPref(property.name, defaultValue)
+        }
+    }
+
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected inner class FloatPrefProvider internal constructor(private val defaultValue: Float) {
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): FloatPref {
+            return FloatPref(property.name, defaultValue)
+        }
+    }
+
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected inner class LongPrefProvider internal constructor(private val defaultValue: Long) {
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): LongPref {
+            return LongPref(property.name, defaultValue)
+        }
+    }
+
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected inner class StringPrefProvider internal constructor(
+        private val defaultValue: String
+    ) {
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): StringPref {
+            return StringPref(property.name, defaultValue)
+        }
+    }
+
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected inner class StringOrNullPrefProvider internal constructor(
+        private val defaultValue: String?
+    ) {
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): StringOrNullPref {
+            return StringOrNullPref(property.name, defaultValue)
+        }
+    }
+
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected inner class StringSetPrefProvider internal constructor(
+        private val defaultValue: Set<String>
+    ) {
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): StringSetPref {
+            return StringSetPref(property.name, defaultValue)
+        }
+    }
+
+    @Deprecated(IMPLICIT_KEY_DEPRECATION, level = DeprecationLevel.WARNING)
+    protected inner class StringSetOrNullPrefProvider internal constructor(
+        private val defaultValue: Set<String>?
+    ) {
+        operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): StringSetOrNullPref {
+            return StringSetOrNullPref(property.name, defaultValue)
+        }
+    }
+
+    companion object {
+        private const val IMPLICIT_KEY_DEPRECATION = "Please specify the key explicitly. " +
+                "The ability to implicitly use the property name as a key will be removed before " +
+                "Splitties 3.0.0 after its deprecation level is raised to error in next version."
+    }
+}
+
+@SuppressLint("CommitPrefEdits")
+sealed class PreferencesBase(
+    name: String,
+    availableAtDirectBoot: Boolean = false,
+    mode: Int = Context.MODE_PRIVATE
+) {
+
+    val prefs: SharedPreferences
+
+    @InternalSplittiesApi
+    fun beginEdit(blocking: Boolean = false) {
+        useCommitForEdit = blocking
+        isEditing = true
+    }
+
+    @InternalSplittiesApi
+    fun endEdit() {
+        if (useCommitForEdit) editor.commit() else editor.apply()
+        isEditing = false
+    }
+
+    @InternalSplittiesApi
+    fun abortEdit() {
+        editor = editor // Invalidates the editor stored in the delegate
+        isEditing = false
+    }
+
+    init {
+        val storageCtx: Context = if (availableAtDirectBoot && SDK_INT > 24) {
+            // Moving the sharedPreferences from is done by the system only if you had it outside
+            // the direct boot available storage or if the device was running Android M or older,
+            // and just got updated.
+            directBootCtx.moveSharedPreferencesFrom(appCtx, name)
+            directBootCtx
+        } else appCtx
+        prefs = storageCtx.getSharedPreferences(name, mode)
+    }
+
+    internal var editor: SharedPreferences.Editor by ResettableLazy { prefs.edit() }
+        private set
+
+    operator fun contains(o: Any) = prefs === o
+
+    private var isEditing = false
+    private var useCommitForEdit = false
+
+    internal fun SharedPreferences.Editor.attemptApply() {
+        if (isEditing) return
+        apply()
+    }
 }
