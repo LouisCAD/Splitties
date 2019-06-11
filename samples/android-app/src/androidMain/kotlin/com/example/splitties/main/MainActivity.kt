6b9603e5e70784f14f5a4273b3d222223670f1c9
Louis CAD <louis.cognault@gmail.com>
2019-06-11 13:49:50 +0200

    Implement sample preferences UI

--- samples/android-app/src/androidMain/kotlin/com/example/splitties/main/MainActivity.kt
+++ samples/android-app/src/androidMain/kotlin/com/example/splitties/main/MainActivity.kt
@@ -12,16 +12,25 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.splitties.R
 import com.example.splitties.about.AboutActivity
 import com.example.splitties.demo.DemoActivity
+import com.example.splitties.extensions.coroutines.collectEachAndCancelPrevious
 import com.example.splitties.extensions.menu.addItem
 import com.example.splitties.extensions.menu.neverShowAsAction
 import com.example.splitties.extensions.toggleNightMode
-import com.example.splitties.prefs.GamePreferences
 import com.example.splitties.permissions.PermissionsExampleActivity
+import com.example.splitties.preferences.SamplePreferences
+import com.example.splitties.preferences.ui.PreferencesBottomSheetDialogFragment
+import com.example.splitties.prefs.GamePreferences
 import com.example.splitties.preview.vibrator.VibrationEffect
 import com.example.splitties.preview.vibrator.vibrate
 import com.example.splitties.sayhello.SayHelloActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import splitties.activities.start
+import splitties.alertdialog.appcompat.alertDialog
+import splitties.alertdialog.appcompat.coroutines.showAndAwaitOkOrDismiss
+import splitties.fragments.showAsync
 import splitties.lifecycle.coroutines.lifecycleScope
 import splitties.preferences.edit
 import splitties.snackbar.longSnack
@@ -47,14 +56,39 @@ class MainActivity : AppCompatActivity() {
                     currentLevel++
                     bossesFought++
                     lastTimePlayed = System.currentTimeMillis()
-                } // TODO: 02/03/2017 Prove this working by moving it to a dedicated activity for prefs
-                // TODO: 02/03/2017 With a UI showing prefs values.
-                //TODO: Use PrefsAdapter in a bottom sheet for real example
+                }
             }
             vibrator.vibrate(vibe)
             ui.root.longSnack(R.string.cant_dislike_md)
         }
         ui.toggleNightModeBtn.onClick { toggleNightMode() }
+        lifecycleScope.launch {
+            val prefs = SamplePreferences()
+            @UseExperimental(ExperimentalCoroutinesApi::class)
+            prefs.enableAnnoyingFeaturesField.valueFlow().collectEachAndCancelPrevious { annoy ->
+                if (annoy) {
+                    if (prefs.showAnnoyingPopupAtLaunch) {
+                        alertDialog(
+                            title = "Launching the annoyance",
+                            message = "Annoying features are enabled and the " +
+                                    "\"Popup at launch\" option is checked."
+                        ).showAndAwaitOkOrDismiss()
+                    }
+                    prefs.showAnnoyingPopupInLoopField.valueFlow()
+                        .collectEachAndCancelPrevious { loop ->
+                            if (loop) while (isActive) {
+                                toast("Next up in 6 seconds")
+                                delay(6000)
+                                alertDialog(
+                                    title = "Looping the annoyance",
+                                    message = "Annoying features are enabled and the " +
+                                            "\"Show annoying popup in loop\" option is checked."
+                                ).showAndAwaitOkOrDismiss()
+                            }
+                        }
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu) = super.onCreateOptionsMenu(menu).also { _ ->
@@ -67,7 +101,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_settings -> toast("No settings yet!").let { true }
+        R.id.action_settings -> showAsync(::PreferencesBottomSheetDialogFragment).let { true }
         R.id.action_about -> start<AboutActivity>().let { true }
         else -> super.onOptionsItemSelected(item)
     }
---
+++ samples/android-app/src/androidMain/kotlin/com/example/splitties/preferences/ui/PreferencesBottomSheetDialogFragment.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package com.example.splitties.preferences.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.launch
+import splitties.lifecycle.coroutines.lifecycleScope
+
+class PreferencesBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = SamplePreferencesUi(requireContext()).apply {
+        viewLifecycleOwner.lifecycleScope.launch { run() }
+    }.root
+}
