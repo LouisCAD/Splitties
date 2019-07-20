c7dfbecefc0bb8baa1136c7874b7f80910351841
Louis CAD <louis.cognault@gmail.com>
2019-07-20 16:10:21 +0200

    Update Create-new-module.kts
    It now updates settings.gradle.kts and offers to copy the module name
    into the clipboard.

--- scripts/Create-new-module.kts
+++ scripts/Create-new-module.kts
@@ -2,9 +2,12 @@
  * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 import java.io.File
 
-//This is NOT a gradle script. It includes .gradle so the IDE recognizes it, but it should only use Kotlin stdlib.
+// To make the IDE recognize this file, copy-paste it in a Scratch File (cmd/ctrl + shift + N).
+
 println("Welcome in Create new module by Louis CAD")
 
 inline fun <R> runOrRetry(block: () -> R): R {
@@ -18,17 +21,28 @@ inline fun <R> runOrRetry(block: () -> R): R {
 val currentDir = File(".")
 val settingsFile = currentDir.resolve("settings.gradle.kts")
 check(settingsFile.exists()) { "No ${settingsFile.name} found!" }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Ask selection of a Gradle template. ////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 val templatesDirectory = currentDir.resolve("scripts").resolve("gradle_templates")
 check(templatesDirectory.exists()) { "Template directory (${templatesDirectory.name}) not found!" }
-val templateDirNames = templatesDirectory.list { file, name ->
+
+val templateDirNames: List<String> = checkNotNull(templatesDirectory.list { file, name ->
     file.isDirectory && name.startsWith('.').not()
-}.asList()
-check(templateDirNames.isNotEmpty()) { "No templates found!" }
+}).asList().also {
+    check(it.isNotEmpty()) { "No templates found!" }
+}
+
 println("Please select a template")
+
 templateDirNames.forEachIndexed { i, fileName ->
     println("${i + 1}. $fileName")
 }
+
 println("Enter the number: ")
+
 val templateIndex = runOrRetry {
     val input = readLine() ?: ""
     val number = input.toIntOrNull() ?: throw IllegalStateException("Please, input a number.")
@@ -36,9 +50,15 @@ val templateIndex = runOrRetry {
         require(it in 1..templateDirNames.size) { "$it is not among the available options." }
     } - 1
 }
-val selectedTemplateDirName = templateDirNames[templateIndex]!!
+val selectedTemplateDirName = templateDirNames[templateIndex]
 val selectedTemplateDir = templatesDirectory.resolve(selectedTemplateDirName)
+
 println("You selected $selectedTemplateDirName\n")
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Ask module name and relative path. /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 val moduleName = runOrRetry {
     println("Please type the name of the module you want to create")
     println("Enter the name: ")
@@ -51,12 +71,20 @@ val moduleName = runOrRetry {
         }
     }
 }
-val destinationDir = currentDir.let {
-    print("Please type the relative path of the new module (target directory, ")
-    println("name of module excluded), or leave empty to use this directory as root.")
-    val relativePathName = (readLine() ?: "").let { if (it.endsWith('/')) it else "$it/" }
-    it.resolve("$relativePathName$moduleName")
+print("Please type the relative path of the new module (target directory, ")
+println("name of module excluded), or leave empty to use this directory as root.")
+
+val relativePathName = (readLine() ?: "").let {
+    if (it.endsWith('/') || it.isEmpty()) it else "$it/"
 }
+
+val destinationDir: File = currentDir.resolve("$relativePathName$moduleName")
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Copy template into destination directory, asking any need value with a € symbol (instead of $) /
+// string template (e.g. €{some key}). /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 check(selectedTemplateDir.copyRecursively(destinationDir)) { "Copy operation didn't succeed" }
 
 /**
@@ -66,8 +94,10 @@ check(selectedTemplateDir.copyRecursively(destinationDir)) { "Copy operation did
  * will happen because the referenced file no longer exists.
  */
 fun File.doRecursively(action: (File) -> File?) {
-    val file = action(this) ?: this
-    if (file.isDirectory) file.listFiles().forEach { it.doRecursively(action) }
+    val newOrCurrentFile = action(this) ?: this
+    if (newOrCurrentFile.isDirectory) {
+        checkNotNull(newOrCurrentFile.listFiles()).forEach { it.doRecursively(action) }
+    }
 }
 
 class LazyMap<K, V>(
@@ -129,7 +159,88 @@ destinationDir.doRecursively { file ->
     null
 }
 
-//TODO("Update settings.gradle.kts file (maybe should prompt the user)")
-//Idea: Prompt copying the module name (with double quotes and column?) to clipboard.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Update settings.gradle.kts to include newly created module. ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+val settingsFileText = settingsFile.readText()
+
+val includeForLoopLineOfCode = "/${relativePathName.removeSuffix("/")}"
+    .replace("/", ":")
+    .let { modulePrefix -> ").forEach { include(\"$modulePrefix:\$it\") }" }
+
+val indexOfTargetLoop = settingsFileText.indexOf(includeForLoopLineOfCode)
+
+val newGradleSettingsFileContent: String = if (indexOfTargetLoop >= 0) {
+    val indexOfTargetArray = settingsFileText.lastIndexOf("arrayOf", startIndex = indexOfTargetLoop)
+    val indexOfFirstArrayElement = settingsFileText.indexOf('\n', startIndex = indexOfTargetArray)
+
+    settingsFileText.replaceRange(
+        startIndex = indexOfFirstArrayElement,
+        endIndex = indexOfTargetLoop,
+        replacement = settingsFileText.substring(
+            startIndex = indexOfFirstArrayElement,
+            endIndex = indexOfTargetLoop
+        ).let { arrayLines ->
+            val newArrayLines = arrayLines.lines() + "    \"$moduleName\","
+            newArrayLines.sorted().joinToString(separator = "\n")
+        }
+    )
+} else {
+
+    val lastIncludeIndex = settingsFileText.lastIndexOfAny(
+        strings = listOf(").forEach { include(\"", "include(")
+    )
+
+    val replacement = """
+                |
+                |
+                |arrayOf(
+                |    "$moduleName",
+                |$includeForLoopLineOfCode
+            """.trimMargin()
+
+    if (lastIncludeIndex >= 0) settingsFileText.indexOf('\n', startIndex = lastIncludeIndex).let {
+        val startIndex = if (it >= 0) it else settingsFileText.lastIndex
+        settingsFileText.replaceRange(
+            startIndex = startIndex,
+            endIndex = startIndex,
+            replacement = replacement
+        )
+    } else settingsFileText.replaceRange(
+        startIndex = settingsFileText.lastIndex,
+        endIndex = settingsFileText.lastIndex,
+        replacement = replacement
+    )
+}
+
+settingsFile.writeText(newGradleSettingsFileContent)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Ask to put module name in clipboard. ///////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+fun putInClipboard(text: String) {
+    Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(text), null)
+}
+
+
+runOrRetry {
+    println("Do you want to put the module name in clipboard?")
+    println("(Press enter without entering anything to skip.)")
+    println()
+    println("1. Yes, with quotes")
+    println("2. Yes, without quotes")
+    println("3. No thanks")
+    when (readLine()!!.toInt()) {
+        1 -> putInClipboard("\"$moduleName\"")
+        2 -> putInClipboard(moduleName)
+        3 -> Unit
+    }
+}
+
+println("We're done! Don't forget to setup dependencies manually if needed.")
+println("Have a nice day!")
+
 //Idea: Support loop prompt for api and implementation dependencies with set of existing values in number choices.
 //Idea: Support optional directories and files.
