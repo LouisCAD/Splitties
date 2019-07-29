e27db321e043a72b568878aebf6f412084dafba7
Louis CAD <louis.cognault@gmail.com>
2019-07-29 13:56:42 +0200

    Minor toggleNightMode improvement

--- samples/android-app/src/androidMain/kotlin/com/example/splitties/extensions/UiMode.kt
+++ samples/android-app/src/androidMain/kotlin/com/example/splitties/extensions/UiMode.kt
@@ -2,23 +2,30 @@
  * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
  */
 
+@file:Suppress("NOTHING_TO_INLINE")
+
 package com.example.splitties.extensions
 
-import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.appcompat.app.AppCompatDelegate.getDefaultNightMode
 import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
-import splitties.exceptions.illegalArg
+import timber.log.Timber
 
-fun Activity.toggleNightMode() {
+inline fun AppCompatActivity.toggleNightMode() {
+    delegate.toggleNightMode()
+}
+
+fun AppCompatDelegate.toggleNightMode() {
     val newNightMode = when (val currentNightMode = getDefaultNightMode()) {
         MODE_NIGHT_YES -> MODE_NIGHT_NO
         MODE_NIGHT_NO, MODE_NIGHT_AUTO, MODE_NIGHT_FOLLOW_SYSTEM -> MODE_NIGHT_YES
-        else -> illegalArg(currentNightMode)
+        else -> MODE_NIGHT_YES.also { Timber.w("Unexpected night mode: $currentNightMode") }
     }
     setDefaultNightMode(newNightMode)
-    recreate()
+    applyDayNight()
 }
