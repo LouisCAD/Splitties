2835387beade737efc64736b05bbf9560d184676
Louis CAD <louis.cognault@gmail.com>
2019-09-10 11:51:42 +0200

    Make sure buildSrc can access Gradle Kotlin DSL

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 val kotlinVersion = "1.3.50" // Don't forget to update in Dependencies.kt too!
 
 dependencies {
-    compileOnly(gradleApi())
+    compileOnly(gradleKotlinDsl())
     implementation("com.android.tools.build:gradle:3.5.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 }
