3879776583bbc52f3f35baec91f88efe10e3134e
Louis CAD <louis.cognault@gmail.com>
2019-09-13 17:30:45 +0200

    Replace FreezableAtomicReference by AtomicReference

--- modules/preferences/src/appleMain/kotlin/splitties/preferences/FrozenDelegate.kt
+++ modules/preferences/src/appleMain/kotlin/splitties/preferences/FrozenDelegate.kt
@@ -4,14 +4,14 @@
 
 package splitties.preferences
 
-import kotlin.native.concurrent.FreezableAtomicReference
+import kotlin.native.concurrent.AtomicReference
 import kotlin.native.concurrent.freeze
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 internal class FrozenDelegate<T>(initialValue: T) : ReadWriteProperty<Any?, T> {
 
-    private val reference = FreezableAtomicReference(initialValue.freeze())
+    private val reference = AtomicReference(initialValue.freeze())
 
     init {
         freeze()
