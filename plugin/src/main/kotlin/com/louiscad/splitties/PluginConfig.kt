90f623f6c379ee623f21e6fdc05a54863200d95f
Jean-Michel Fayard <jean-michel.fayard@akelius.de>
2019-10-05 10:06:43 +0200

    Plugin documentation and resolutionStrategy

---
+++ plugin/src/main/kotlin/com/louiscad/splitties/PluginConfig.kt
@@ -0,0 +1,8 @@
+package com.louiscad.splitties
+
+
+object PluginConfig {
+    const val GROUP = "com.louiscad.splitties"
+    const val GRADLE_PROPERTY = "version.$GROUP"
+    const val SPLITTIES_VERSION = "3.0.0-dev-034"
+}
--- plugin/src/main/kotlin/com/louiscad/splitties/SplittiesPlugin.kt
+++ plugin/src/main/kotlin/com/louiscad/splitties/SplittiesPlugin.kt
@@ -7,8 +7,32 @@ import org.gradle.api.Project
 open class SplittiesPlugin : Plugin<Project> {
 
     override fun apply(project: Project) = project.run {
-        println("Hello World SplittiesPlugin")
+        splittiesVersionComesFromGradleProperties()
         Unit
     }
 
+}
+
+fun Project.splittiesVersionComesFromGradleProperties() = with(PluginConfig) {
+    val splittiesVersion = findProperty("version.") as? String ?: errorMissingProperty()
+    allprojects {
+        configurations.all {
+            if (name.contains("copy")) return@all
+            resolutionStrategy {
+                eachDependency {
+                    if (requested.group == GROUP)
+                        useVersion(splittiesVersion)
+                }
+            }
+        }
+    }
+}
+
+fun errorMissingProperty(): Nothing = with(PluginConfig) {
+    error("""
+Splitties version not configured. 
+Please add in "gradle.properties" something like:
+   $GRADLE_PROPERTY=$SPLITTIES_VERSION
+        """.trimIndent()
+    )
 }
