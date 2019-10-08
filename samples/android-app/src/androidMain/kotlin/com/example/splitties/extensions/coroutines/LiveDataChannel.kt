566679caf33a448634c72f9eb66656e333de6614
Louis CAD <louis.cognault@gmail.com>
2019-10-08 21:08:48 +0200

    Add LiveData interop with coroutines Flow

--- samples/android-app/src/androidMain/kotlin/com/example/splitties/extensions/coroutines/LiveDataChannel.kt
+++ samples/android-app/src/androidMain/kotlin/com/example/splitties/extensions/coroutines/LiveDataChannel.kt
@@ -7,21 +7,40 @@ package com.example.splitties.extensions.coroutines
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import splitties.arch.lifecycle.LifecycleObserver
+import splitties.coroutines.offerCatching
+import splitties.lifecycle.coroutines.MainAndroid
+import splitties.mainthread.checkMainThread
 
 @ExperimentalCoroutinesApi
 fun <T> LifecycleOwner.channelOf(
     liveData: LiveData<T>
-): ReceiveChannel<T?> = Channel<T?>(Channel.CONFLATED).also {
-    val observer = Observer<T?> { t -> it.offer(t) }
-    liveData.observe(this, observer)
-    it.invokeOnClose { liveData.removeObserver(observer) }
-    lifecycle.addObserver(object : LifecycleObserver {
-        override fun onDestroy(owner: LifecycleOwner) {
-            it.close()
-        }
-    })
+): ReceiveChannel<T?> {
+    checkMainThread()
+    return Channel<T?>(Channel.CONFLATED).also {
+        val observer = Observer<T?> { t -> it.offerCatching(t) }
+        liveData.observe(this, observer)
+        it.invokeOnClose { liveData.removeObserver(observer) }
+        lifecycle.addObserver(object : LifecycleObserver {
+            override fun onDestroy(owner: LifecycleOwner) {
+                it.close()
+            }
+        })
+    }
 }
+
+@UseExperimental(ExperimentalCoroutinesApi::class)
+fun <T> LifecycleOwner.flowOf(liveData: LiveData<T>): Flow<T?> = flow {
+    @UseExperimental(FlowPreview::class)
+    emitAll(channelOf(liveData).consumeAsFlow())
+}.flowOn(Dispatchers.MainAndroid)
