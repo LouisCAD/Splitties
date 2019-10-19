38047c4db57d1a3872f8d9b4d0ed7d76f6ae6399
Jean-Michel Fayard <jmfayard@gmail.com>
2019-10-19 19:55:54 +0200

    plugin: SPLITTIES_VERSION = "3.0.0-alpha06"

--- plugin/src/main/kotlin/com/louiscad/splitties/PluginConfig.kt
+++ plugin/src/main/kotlin/com/louiscad/splitties/PluginConfig.kt
@@ -4,5 +4,5 @@ package com.louiscad.splitties
 object PluginConfig {
     const val GROUP = "com.louiscad.splitties"
     const val GRADLE_PROPERTY = "version.$GROUP"
-    const val SPLITTIES_VERSION = "2.1.1"
+    const val SPLITTIES_VERSION = "3.0.0-alpha06"
 }
