22d4938ca813f3953df2838569847a91635b5e18
Louis CAD <louis.cognault@gmail.com>
2020-01-19 21:36:31 +0100

    Make dip and dp return the type of their parameter

--- modules/dimensions/src/androidMain/kotlin/splitties/dimensions/Dimensions.kt
+++ modules/dimensions/src/androidMain/kotlin/splitties/dimensions/Dimensions.kt
@@ -10,7 +10,11 @@ import android.content.Context
 import android.view.View
 
 inline fun Context.dip(value: Int): Int = (value * resources.displayMetrics.density).toInt()
-inline fun Context.dp(value: Int): Float = (value * resources.displayMetrics.density)
-
+inline fun Context.dip(value: Float): Float = (value * resources.displayMetrics.density)
 inline fun View.dip(value: Int): Int = context.dip(value)
-inline fun View.dp(value: Int): Float = context.dp(value)
+inline fun View.dip(value: Float): Float = context.dip(value)
+
+inline fun Context.dp(value: Int): Int = dip(value)
+inline fun Context.dp(value: Float): Float = dip(value)
+inline fun View.dp(value: Int): Int = context.dip(value)
+inline fun View.dp(value: Float): Float = context.dip(value)
