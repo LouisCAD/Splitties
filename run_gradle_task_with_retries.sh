907cab587ff0db41caab0753ee6ec39f09d7f27e
Louis CAD <louis.cognault@gmail.com>
2020-08-02 17:54:12 +0400

    Fix run_gradle_task_with_retries.sh

--- run_gradle_task_with_retries.sh
+++ run_gradle_task_with_retries.sh
@@ -1,4 +1,4 @@
-./gradlew "$1 --scan -PbuildScan.termsOfServiceAgree=yes" ||
-./gradlew "$1 --scan -PbuildScan.termsOfServiceAgree=yes" || # Retry if failed
-./gradlew "$1 --scan -PbuildScan.termsOfServiceAgree=yes" # Retry again
+./gradlew "$1" --scan -PbuildScan.termsOfServiceAgree=yes ||
+./gradlew "$1" --scan -PbuildScan.termsOfServiceAgree=yes || # Retry if failed
+./gradlew "$1" --scan -PbuildScan.termsOfServiceAgree=yes # Retry again
 exit $? # Exit with the code of the last command run
