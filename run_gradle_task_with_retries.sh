346c7930ae9069cc863c807ac8eb1e2eca8adf74
Louis CAD <louis.cognault@gmail.com>
2020-08-02 18:36:09 +0400

    Fix arguments forwarding in run_gradle_task_with_retries.sh

--- run_gradle_task_with_retries.sh
+++ run_gradle_task_with_retries.sh
@@ -1,4 +1,4 @@
-./gradlew "$1" --scan -PbuildScan.termsOfServiceAgree=yes ||
-./gradlew "$1" --scan -PbuildScan.termsOfServiceAgree=yes || # Retry if failed
-./gradlew "$1" --scan -PbuildScan.termsOfServiceAgree=yes # Retry again
+./gradlew "$*" --scan -PbuildScan.termsOfServiceAgree=yes ||
+./gradlew "$*" --scan -PbuildScan.termsOfServiceAgree=yes || # Retry if failed
+./gradlew "$*" --scan -PbuildScan.termsOfServiceAgree=yes # Retry again
 exit $? # Exit with the code of the last command run
