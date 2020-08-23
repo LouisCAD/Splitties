9b15021e9bde47fefebe5c5de88e6cd0a1740a29
Louis CAD <louis.cognault@gmail.com>
2020-08-23 16:47:01 +0200

    Add slider, rangeSlider and shapeableImageView in Views DSL Material

--- modules/views-dsl-material/src/androidMain/kotlin/splitties/views/dsl/material/Views.kt
+++ modules/views-dsl-material/src/androidMain/kotlin/splitties/views/dsl/material/Views.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2019-2020 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
  */
 package splitties.views.dsl.material
 
@@ -9,13 +9,16 @@ import androidx.annotation.IdRes
 import androidx.annotation.StyleRes
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.slider.RangeSlider
+import com.google.android.material.slider.Slider
 import splitties.views.dsl.core.NO_THEME
 import splitties.views.dsl.core.Ui
 import splitties.views.dsl.core.view
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
-// FloatingActionButton
+//region FloatingActionButton
 
 inline fun Context.floatingActionButton(
     @IdRes id: Int = View.NO_ID,
@@ -44,7 +47,9 @@ inline fun Ui.floatingActionButton(
     return ctx.floatingActionButton(id, theme, initView)
 }
 
-// MaterialCardView
+//endregion
+
+//region MaterialCardView
 
 inline fun Context.materialCardView(
     @IdRes id: Int = View.NO_ID,
@@ -72,3 +77,95 @@ inline fun Ui.materialCardView(
     contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
     return ctx.materialCardView(id, theme, initView)
 }
+
+// endregion
+
+//region ShapeableImageView
+inline fun Context.shapeableImageView(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: ShapeableImageView.() -> Unit = {}
+): ShapeableImageView {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return view(id, theme, initView)
+}
+
+inline fun View.shapeableImageView(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: ShapeableImageView.() -> Unit = {}
+): ShapeableImageView {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return context.shapeableImageView(id, theme, initView)
+}
+
+inline fun Ui.shapeableImageView(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: ShapeableImageView.() -> Unit = {}
+): ShapeableImageView {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return ctx.shapeableImageView(id, theme, initView)
+}
+
+// endregion
+
+//region Slider
+inline fun Context.slider(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: Slider.() -> Unit = {}
+): Slider {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return view(id, theme, initView)
+}
+
+inline fun View.slider(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: Slider.() -> Unit = {}
+): Slider {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return context.slider(id, theme, initView)
+}
+
+inline fun Ui.slider(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: Slider.() -> Unit = {}
+): Slider {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return ctx.slider(id, theme, initView)
+}
+
+// endregion
+
+//region RangeSlider
+inline fun Context.rangeSlider(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: RangeSlider.() -> Unit = {}
+): RangeSlider {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return view(id, theme, initView)
+}
+
+inline fun View.rangeRangeSlider(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: RangeSlider.() -> Unit = {}
+): RangeSlider {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return context.rangeSlider(id, theme, initView)
+}
+
+inline fun Ui.rangeRangeSlider(
+    @IdRes id: Int = View.NO_ID,
+    @StyleRes theme: Int = NO_THEME,
+    initView: RangeSlider.() -> Unit = {}
+): RangeSlider {
+    contract { callsInPlace(initView, InvocationKind.EXACTLY_ONCE) }
+    return ctx.rangeSlider(id, theme, initView)
+}
+
+// endregion
