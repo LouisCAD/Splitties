5ef9ce54ce5e55accf1857407d0e229c6de526c4
Louis CAD <louis.cognault@gmail.com>
2020-09-12 17:36:48 +0200

    Update/fix publication-checker
    Ensure the libraries are added only to the sourceSets of
    the supported targets.

--- tools/publication-checker/build.gradle.kts
+++ tools/publication-checker/build.gradle.kts
@@ -4,6 +4,11 @@
 
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.*
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import retrofit2.Response
 import kotlin.time.*
 
@@ -27,13 +32,15 @@ val bintrayRepoName = bintrayProperty(if (isDevVersion) "repo.dev" else "repo.re
 val bintrayPackageName = bintrayProperty("package")
 
 repositories {
+    mavenLocal().ensureGroups("com.louiscad.splitties")
+    return@repositories
     maven("https://dl.bintray.com/$bintrayUsername/$bintrayRepoName/") {
         name = "bintray/$bintrayUsername/$bintrayRepoName"
         credentials {
             username = bintrayUsername
             password = bintrayApiKey
         }
-    }
+    }.ensureGroups("com.louiscad.splitties")
 }
 
 tasks.matching { it.name.startsWith("lint") }.configureEach { enabled = false }
@@ -42,10 +49,22 @@ android {
     setDefaults()
 }
 
+fun KotlinTarget.targetsSamePlatformAs(other: KotlinTarget): Boolean = when (this) {
+    is KotlinNativeTarget -> other is KotlinNativeTarget && konanTarget == other.konanTarget
+    else -> platformType == other.platformType
+}
+
+val KotlinTarget.mainSourceSet get() = when {
+    platformType == androidJvm && compilations.isEmpty() -> {
+        project.the<KotlinMultiplatformExtension>().sourceSets.androidMain
+    }
+    else -> compilations.getByName("main").defaultSourceSet
+}
+
 kotlin {
     android()
     jvm()
-    js()
+    js { browser(); nodejs() }
 
     macosX64()
     iosArm32(); iosArm64(); iosX64()
@@ -54,44 +73,52 @@ kotlin {
     mingw(x64 = true)
     linux(x64 = true)
 
+    val group = "com.louiscad.splitties"
+    val version = thisLibraryVersion
+
     sourceSets {
-        val group = "com.louiscad.splitties"
-        val version = thisLibraryVersion
-
-        commonMain.dependencies {
-
-            rootProject.project(":modules").subprojects.filter {
-                it.plugins.hasPlugin("org.jetbrains.kotlin.multiplatform")
-            }.forEach {
-                implementation("$group:splitties-${it.name}") {
-                    version { strictly(version) }
-                }
+        commonMain {
+            dependencies {
+                // Needs to be put back if published with HMPP-only,
+                // e.g. without this property: kotlin.mpp.enableCompatibilityMetadataVariant=true,
+                // Otherwise, the dependencies task fails with an obscure error:
+                // "Couldn't resolve metadata artifact for ModuleDependencyIdentifier(groupId=org.jetbrains.kotlinx, moduleId=kotlinx-coroutines-core) in configuration ':tools:publication-checker:iosArm32CompileKlibraries'"
+                //TODO: Report the issue before HMPP becomes the default.
+                //implementation(KotlinX.coroutines.core)
             }
+        }
+    }
 
-            rootProject.project(":fun-packs").subprojects.filter {
-                it.plugins.hasPlugin("org.jetbrains.kotlin.multiplatform")
-            }.forEach {
-                implementation("$group:splitties-fun-pack-${it.name}") {
-                    version { strictly(version) }
+    rootProject.project(":modules").subprojects {
+        if (plugins.hasPlugin("org.jetbrains.kotlin.multiplatform").not()) return@subprojects
+        val splitTargets = the<KotlinMultiplatformExtension>().targets.also {
+            check(it.isNotEmpty())
+        }
+        targets.forEach { target ->
+            if (target.platformType == common) return@forEach
+            val sourceSet: KotlinSourceSet = target.mainSourceSet
+            if (splitTargets.any { it.targetsSamePlatformAs(target) }) {
+                sourceSet.dependencies {
+                    implementation("$group:splitties-${project.name}") {
+                        version { strictly(version) }
+                    }
                 }
             }
         }
-
-        androidMain.dependencies {
-
-            rootProject.project(":modules").subprojects.filter {
-                it.plugins.hasPlugin("org.jetbrains.kotlin.android")
-            }.forEach {
-                implementation("$group:splitties-${it.name}") {
-                    version { strictly(version) }
-                }
-            }
-
-            rootProject.project(":fun-packs").subprojects.filter {
-                it.plugins.hasPlugin("org.jetbrains.kotlin.android")
-            }.forEach {
-                implementation("$group:splitties-fun-pack-${it.name}") {
-                    version { strictly(version) }
+    }
+    rootProject.project(":fun-packs").subprojects {
+        if (plugins.hasPlugin("org.jetbrains.kotlin.multiplatform").not()) return@subprojects
+        val splitTargets = the<KotlinMultiplatformExtension>().targets.also {
+            check(it.isNotEmpty())
+        }
+        targets.forEach { target ->
+            if (target.platformType == common) return@forEach
+            val sourceSet: KotlinSourceSet = target.mainSourceSet
+            if (splitTargets.any { it.targetsSamePlatformAs(target) }) {
+                sourceSet.dependencies {
+                    implementation("$group:splitties-fun-pack-${project.name}") {
+                        version { strictly(version) }
+                    }
                 }
             }
         }
