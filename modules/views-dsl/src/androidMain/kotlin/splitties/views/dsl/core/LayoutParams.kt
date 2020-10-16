d229b22318b58cf7752d55e10128b96652b6afbc
Mike <silizium@ok.de>
2020-10-16 16:02:57 +0300

    replaced inline getter with const val for match/wrap

--- modules/views-dsl/src/androidMain/kotlin/splitties/views/dsl/core/LayoutParams.kt
+++ modules/views-dsl/src/androidMain/kotlin/splitties/views/dsl/core/LayoutParams.kt
@@ -12,13 +12,11 @@ import android.view.ViewGroup
  * visible inside [ViewGroup]s.
  */
 @Suppress("unused")
-inline val View.matchParent
-    get() = ViewGroup.LayoutParams.MATCH_PARENT
+const val View.matchParent: Int = ViewGroup.LayoutParams.MATCH_PARENT
 
 /**
  * **A LESS CAPITALIZED ALIAS** to [ViewGroup.LayoutParams.WRAP_CONTENT] that is only
  * visible inside [ViewGroup]s.
  */
 @Suppress("unused")
-inline val View.wrapContent
-    get() = ViewGroup.LayoutParams.WRAP_CONTENT
+const val View.wrapContent: Int = ViewGroup.LayoutParams.WRAP_CONTENT
