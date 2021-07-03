70d5c973b180a134c4829fe2ec85c87d62c8dcdd
Louis CAD <louis.cognault@gmail.com>
2021-07-04 01:11:12 +0200

    Remove no longer needed isDevVersion extension property

--- buildSrc/src/main/kotlin/config/ProjectVersions.kt
+++ buildSrc/src/main/kotlin/config/ProjectVersions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2019-2021 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
  */
 
 @file:Suppress("PackageDirectoryMismatch")
@@ -19,5 +19,3 @@ val Project.thisLibraryVersion: String
             rootProject.file("libraries_version.txt").readLines().first()
         } as String
     }
-
-val Project.isDevVersion get() = thisLibraryVersion.contains("-dev-")
