e0f5a8a696eb3747b3855036868224b50a7e7bd3
Louis CAD <louis.cognault@gmail.com>
2021-08-05 13:03:09 +0200

    Add CompleteKotlin
    This will allow people not on macOS to still get IDE analysis for code
    that relies on Darwin APIs (iOS/macOS/watchOS/tvOS).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,7 @@ pluginManagement {
 plugins {
     id("com.gradle.enterprise").version("3.6.3")
     id("de.fayard.refreshVersions") version "0.10.1"
+    id("com.louiscad.complete-kotlin") version "1.0.0"
 }
 
 gradleEnterprise {
