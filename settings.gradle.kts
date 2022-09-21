a19fff667d8600b36fc21521b5317068b5074666
Louis CAD <louis.cognault@gmail.com>
2022-09-21 19:21:08 +0200

    Gradle Enterprise 3.11.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise").version("3.8.1")
+    id("com.gradle.enterprise").version("3.11.1")
     id("de.fayard.refreshVersions") version "0.50.1"
 }
 
