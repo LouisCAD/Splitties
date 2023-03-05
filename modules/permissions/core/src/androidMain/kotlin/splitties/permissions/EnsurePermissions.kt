653b2dcab5e1c106a88a75775022be1f3e7a178e
Louis CAD <louis.cognault@gmail.com>
2023-03-05 22:57:01 +0100

    Add ensureAllPermissions overload accepting a list

--- modules/permissions/core/src/androidMain/kotlin/splitties/permissions/EnsurePermissions.kt
+++ modules/permissions/core/src/androidMain/kotlin/splitties/permissions/EnsurePermissions.kt
@@ -86,6 +86,82 @@ suspend inline fun ComponentActivity.ensureAllPermissions(
     showRationaleBeforeFirstAsk: Boolean = true,
     askOpenSettingsOrReturn: () -> Boolean,
     returnOrThrowBlock: () -> Nothing
+) = ensureAllPermissions(
+    permissions = permissions,
+    showRationaleAndContinueOrReturn = showRationaleAndContinueOrReturn,
+    showRationaleBeforeFirstAsk = showRationaleBeforeFirstAsk,
+    askOpenSettingsOrReturn = askOpenSettingsOrReturn,
+    returnOrThrowBlock = returnOrThrowBlock
+)
+
+/**
+ * Returns immediately if the [permissions] are all already granted or if the device API level is
+ * lower than 23 (runtime permissions were introduced in API level 23, and are granted at
+ * app installation time on older Android API levels).
+ *
+ * In the [showRationaleAndContinueOrReturn] function, you should show the rationale for the
+ * permission you are requesting, await for user acknowledgement, and return `true` if the user
+ * agreed to continue. If you return `false` in that function, the [returnOrThrowBlock] function
+ * will be executed and the permission won't be requested again.
+ *
+ * The [showRationaleBeforeFirstAsk] parameter is true by default. If the context in which the
+ * permission is asked makes it absolutely obvious why it is required, then you should set it to
+ * `false`. Such an example is asking for camera permission when the user pressed a button to start
+ * recording a video.
+ *
+ * The [askOpenSettingsOrReturn] function is executed if the user ticked the "do not ask again"
+ * checkbox and denied the permission. You can show a relevant message to the user if needed and
+ * await for an interaction before returning a value. If you return `true`, the application details
+ * will be opened in the device settings, and the user will be able to enable the permissions.
+ * When the user comes back into your app, this function (`ensurePermission`) will return
+ * immediately if the permission that was asked has been granted. Otherwise, the function will
+ * show the rationale again (calling [showRationaleAndContinueOrReturn]), restarting the permission
+ * request process. If you return `false`, the [returnOrThrowBlock] function will be executed and
+ * the permission won't be requested again.
+ *
+ * The [returnOrThrowBlock] function is inline (like all the other functions you pass here), so if
+ * you put a `return` expression in it, the function at call site will return. Note that the type
+ * of a `return` expression is always [Nothing], regardless of the type of what is returned.
+ * You can also throw if that better suits the way you handle the user cancelling the permission
+ * grant.
+ *
+ * The passed [ComponentActivity] is used for 3 things:
+ *
+ * 1. To check whether we should show the rationale for the permission
+ * request, and is also used in the case we need to open the application details settings to let the
+ * user grant the permission in case the do not ask again checkbox was ticked.
+ *
+ * 2. To get a [Lifecycle] that will be used to await the resumed state before requesting the
+ * permission, and will also be used to await user coming back from application details settings
+ * if that path was required.
+ *
+ * 3. To get the [ActivityResultRegistry] that allows performing the permission request, and get
+ * the result.
+ */
+@ExperimentalSplittiesApi
+suspend inline fun ComponentActivity.ensureAllPermissions(
+    permissions: List<String>,
+    showRationaleAndContinueOrReturn: () -> Boolean,
+    showRationaleBeforeFirstAsk: Boolean = true,
+    askOpenSettingsOrReturn: () -> Boolean,
+    returnOrThrowBlock: () -> Nothing
+) = ensureAllPermissions(
+    permissions = permissions.toTypedArray(),
+    showRationaleAndContinueOrReturn = showRationaleAndContinueOrReturn,
+    showRationaleBeforeFirstAsk = showRationaleBeforeFirstAsk,
+    askOpenSettingsOrReturn = askOpenSettingsOrReturn,
+    returnOrThrowBlock = returnOrThrowBlock
+)
+
+@PublishedApi
+@JvmName("ensurePermissions")
+@ExperimentalSplittiesApi
+internal suspend inline fun ComponentActivity.ensureAllPermissions(
+    permissions: Array<out String>,
+    showRationaleAndContinueOrReturn: () -> Boolean,
+    showRationaleBeforeFirstAsk: Boolean = true,
+    askOpenSettingsOrReturn: () -> Boolean,
+    returnOrThrowBlock: () -> Nothing
 ) {
     var askCount = 0
     if (SDK_INT < 23) return
