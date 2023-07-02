17c93d5038999b49acc00a583279ab66dcdaf814
Louis CAD <louis.cognault@gmail.com>
2023-07-02 19:27:16 +0200

    Remove unneeded suppress annotation

--- modules/checkedlazy/src/androidMain/kotlin/splitties/checkedlazy/CheckedLazy.kt
+++ modules/checkedlazy/src/androidMain/kotlin/splitties/checkedlazy/CheckedLazy.kt
@@ -2,8 +2,6 @@
  * Copyright 2019 Louis Cognault Ayeva Derman. Use of this source code is governed by the Apache 2.0 license.
  */
 
-@file:Suppress("NOTHING_TO_INLINE")
-
 package splitties.checkedlazy
 
 /**
