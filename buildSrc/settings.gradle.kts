7ca8d29b41a7f0eea6962d43c4ac6eb40855ef92
Louis CAD <louis.cognault@gmail.com>
2023-09-27 01:09:51 +0200

    refreshVersions 0.60.2

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("de.fayard.refreshVersions") version "0.51.0"
+        id("de.fayard.refreshVersions") version "0.60.2"
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.enterprise").version("3.12.3")
-    id("de.fayard.refreshVersions") version "0.51.0"
+    id("de.fayard.refreshVersions") version "0.60.2"
 }
 
 gradleEnterprise {
